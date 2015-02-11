081be949a0f1b64d21fe07cc51606f3ee41efcbb
KaiXinXiaoLei <huleilei1@huawei.com>
2015-02-11 17:20:56 +0800

    dependent-jar-uris  support  list type

--- job-server/src/spark.jobserver/JobManagerActor.scala
+++ job-server/src/spark.jobserver/JobManagerActor.scala
@@ -40,8 +40,8 @@ object JobManagerActor {
  * {{{
  *  num-cpu-cores = 4         # Total # of CPU cores to allocate across the cluster
  *  memory-per-node = 512m    # -Xmx style memory string for total memory to use for executor on one node
- *  dependent-jar-uris = "local://opt/foo/my-foo-lib.jar"
- *                            # URIs for dependent jars to load for entire context. URIs of jars Separated by ','
+ *  dependent-jar-uris = ["local://opt/foo/my-foo-lib.jar"]
+ *                            # URIs for dependent jars to load for entire context.
  *  max-jobs-per-context = 4  # Max # of jobs to run at the same time
  *  spark.mesos.coarse = true  # per-context, rather than per-job, resource allocation
  *  rdd-ttl = 24 h            # time-to-live for RDDs in a SparkContext.  Don't specify = forever
