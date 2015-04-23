1039cd4d386c01b362cf55363e915b66168a18cf
Ankit Soni <ankit.soni@applovin.com>
2015-04-22 17:10:53 -0700

    factor out env setup for server and manager start scripts

--- bin/server_start.sh
+++ bin/server_start.sh
@@ -24,63 +24,7 @@ JAVA_OPTS="-Xmx5g -XX:MaxDirectMemorySize=512M
 
 MAIN="spark.jobserver.JobServer"
 
-conffile=$(ls -1 $appdir/*.conf | head -1)
-if [ -z "$conffile" ]; then
-  echo "No configuration file found"
-  exit 1
-fi
-
-if [ -f "$appdir/settings.sh" ]; then
-  . $appdir/settings.sh
-else
-  echo "Missing $appdir/settings.sh, exiting"
-  exit 1
-fi
-
-if [ -z "$SPARK_HOME" ]; then
-  echo "Please set SPARK_HOME or put it in $appdir/settings.sh first"
-  exit 1
-fi
-
-if [ -z "$SPARK_CONF_DIR" ]; then
-  SPARK_CONF_DIR=$SPARK_HOME/conf
-fi
-
-# Pull in other env vars in spark config, such as MESOS_NATIVE_LIBRARY
-. $SPARK_CONF_DIR/spark-env.sh
-
-pidFilePath=$appdir/$PIDFILE
-
-if [ -f "$pidFilePath" ] && kill -0 $(cat "$pidFilePath"); then
-   echo 'Job server is already running'
-   exit 1
-fi
-
-if [ -z "$LOG_DIR" ]; then
-  LOG_DIR=/tmp/job-server
-  echo "LOG_DIR empty; logging will go to $LOG_DIR"
-fi
-mkdir -p $LOG_DIR
-
-LOGGING_OPTS="-Dlog4j.configuration=log4j-server.properties
-              -DLOG_DIR=$LOG_DIR"
-
-# For Mesos
-CONFIG_OVERRIDES=""
-if [ -n "$SPARK_EXECUTOR_URI" ]; then
-  CONFIG_OVERRIDES="-Dspark.executor.uri=$SPARK_EXECUTOR_URI "
-fi
-# For Mesos/Marathon, use the passed-in port
-if [ "$PORT" != "" ]; then
-  CONFIG_OVERRIDES+="-Dspark.jobserver.port=$PORT "
-fi
-
-# This needs to be exported for standalone mode so drivers can connect to the Spark cluster
-export SPARK_HOME
-
-# job server jar needs to appear first so its deps take higher priority
-# need to explicitly include app dir in classpath so logging configs can be found
-CLASSPATH="$appdir:$appdir/spark-job-server.jar:$($SPARK_HOME/bin/compute-classpath.sh)"
+. $appdir/setenv.sh
 
 exec java -cp $CLASSPATH $GC_OPTS $JAVA_OPTS $LOGGING_OPTS $CONFIG_OVERRIDES $MAIN $conffile 2>&1 &
 echo $! > $pidFilePath
