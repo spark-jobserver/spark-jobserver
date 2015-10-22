12ffefbd21fcef856c5593760b2a563d02853765
David Durst <davidbdurst@gmail.com>
2015-10-21 22:35:25 -0400

    made ec2_destory.sh handle SPARK_VERSION variable

--- bin/ec2_destroy.sh
+++ bin/ec2_destroy.sh
@@ -4,4 +4,4 @@ bin=`cd "$bin"; pwd`
 
 . "$bin"/../config/user-ec2-settings.sh
 
-"$bin"/../spark-1.5.0-bin-hadoop2.6/ec2/spark-ec2 destroy $CLUSTER_NAME
+"$bin"/../spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION/spark-ec2 destroy $CLUSTER_NAME
