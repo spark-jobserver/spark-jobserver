c2b3fa168140c7a46bf5956a9ba382067b735371
TimMaltGermany <dw-0@gmx.net>
2016-03-05 16:16:41 +0100

    NamedObjects: improved test

--- job-server-extras/test/spark.jobserver/NamedObjectsJobSpec.scala
+++ job-server-extras/test/spark.jobserver/NamedObjectsJobSpec.scala
@@ -43,7 +43,7 @@ class NamedObjectsJobSpec extends JobSpecBase(JobManagerSpec.getNewSystem) {
       names2 should not contain("df1")
 
       //clean-up
-      manager ! JobManagerActor.StartJob("demo", jobName, emptyConfig.withValue(NamedObjectsTestJobConfig.DELETE, ConfigValueFactory.fromIterable(names.toList))
+      manager ! JobManagerActor.StartJob("demo", jobName, emptyConfig.withValue(NamedObjectsTestJobConfig.DELETE, ConfigValueFactory.fromIterable(List("rdd1")))
         .withValue(NamedObjectsTestJobConfig.CREATE_RDD, ConfigValueFactory.fromAnyRef(false)),
         errorEvents ++ syncEvents)
       val JobResult(_, names3: Array[String]) = expectMsgClass(classOf[JobResult])
@@ -74,8 +74,8 @@ class NamedObjectsJobSpec extends JobSpecBase(JobManagerSpec.getNewSystem) {
       names2 should equal(names)
       
       //clean-up
-      manager ! JobManagerActor.StartJob("demo", jobName, emptyConfig.withValue(NamedObjectsTestJobConfig.DELETE, ConfigValueFactory.fromIterable(names.toList))
-        .withValue(NamedObjectsTestJobConfig.CREATE_RDD, ConfigValueFactory.fromAnyRef(false)),
+      manager ! JobManagerActor.StartJob("demo", jobName, emptyConfig.withValue(NamedObjectsTestJobConfig.DELETE, 
+          ConfigValueFactory.fromIterable(List("df1"))),
         errorEvents ++ syncEvents)
       val JobResult(_, names3: Array[String]) = expectMsgClass(classOf[JobResult])
     }
@@ -91,7 +91,9 @@ class NamedObjectsJobSpec extends JobSpecBase(JobManagerSpec.getNewSystem) {
         .withValue(NamedObjectsTestJobConfig.CREATE_RDD, ConfigValueFactory.fromAnyRef(true)),
         errorEvents ++ syncEvents)
       val JobResult(_, names: Array[String]) = expectMsgClass(classOf[JobResult])
-      names should equal(Array("rdd1", "df1"))
+      
+      names should contain("rdd1")
+      names should contain("df1")
 
       manager ! JobManagerActor.StartJob("demo", jobName, emptyConfig.withValue(NamedObjectsTestJobConfig.CREATE_DF, ConfigValueFactory.fromAnyRef(false))
         .withValue(NamedObjectsTestJobConfig.CREATE_RDD, ConfigValueFactory.fromAnyRef(false)),
@@ -99,6 +101,12 @@ class NamedObjectsJobSpec extends JobSpecBase(JobManagerSpec.getNewSystem) {
       val JobResult(_, names2: Array[String]) = expectMsgClass(classOf[JobResult])
 
       names2 should equal(names)
+      
+      //clean-up
+      manager ! JobManagerActor.StartJob("demo", jobName, emptyConfig.withValue(NamedObjectsTestJobConfig.DELETE,
+          ConfigValueFactory.fromIterable(List("rdd1", "df1"))),
+        errorEvents ++ syncEvents)
+      val JobResult(_, names3: Array[String]) = expectMsgClass(classOf[JobResult])
     }
   }
 
