spark-jobserver/spark-jobserver

commit f1218074a37eff18ba37983d9887075ff5aacbb9

First version of slick upgrade, tests pass

job-server/src/spark.jobserver/JobInfoActor.scala
@@ -1,13 +1,13 @@
 package spark.jobserver
 
+import scala.concurrent.Await
+
 import akka.actor.ActorRef
 import akka.pattern.ask
 import akka.util.Timeout
 import com.typesafe.config.Config
 import ooyala.common.akka.InstrumentedActor
-import scala.concurrent.Await
-import spark.jobserver.ContextSupervisor.{GetContext, StartAdHocContext}
-import spark.jobserver.io.JobDAO
+import spark.jobserver.io.{JobDAO, JobInfo}
 
 object JobInfoActor {
   // Requests
@@ -21,10 +21,11 @@ object JobInfoActor {
 }
 
 class JobInfoActor(jobDao: JobDAO, contextSupervisor: ActorRef) extends InstrumentedActor {
-  import CommonMessages._
-  import JobInfoActor._
   import scala.concurrent.duration._
   import scala.util.control.Breaks._
+
+  import CommonMessages._
+  import JobInfoActor._
   import context.dispatcher       // for futures to work
 
   // Used in the asks (?) below to request info from contextSupervisor and resultActor
@@ -35,15 +36,15 @@ class JobInfoActor(jobDao: JobDAO, contextSupervisor: ActorRef) extends Instrume
       sender ! jobDao.getJobInfos(limit.get)
 
     case GetJobStatus(jobId) =>
-      val jobInfo = jobDao.getJobInfo(jobId)
-      val resp = if (!jobInfo.isDefined) NoSuchJobId else jobInfo.get
+      val jobInfo = Await.result(jobDao.getJobInfo(jobId), 60 seconds)
+      val resp = if (jobInfo.nonEmpty) NoSuchJobId else jobInfo.get
       sender ! resp
 
     case GetJobResult(jobId) =>
       breakable {
-        val jobInfo = jobDao.getJobInfo(jobId)
+        val jobInfo: Option[JobInfo] = Await.result(jobDao.getJobInfo(jobId), 60 seconds)
 
-        if (!jobInfo.isDefined) {
+        if (jobInfo.nonEmpty) {
           sender ! NoSuchJobId
           break
         }
@@ -61,13 +62,14 @@ class JobInfoActor(jobDao: JobDAO, contextSupervisor: ActorRef) extends Instrume
         val resultActor = Await.result(future, 3 seconds)
 
         val receiver = sender // must capture the sender since callbacks are run in a different thread
-        for (result <- (resultActor ? GetJobResult(jobId))) {
+        for (result <- resultActor ? GetJobResult(jobId)) {
           receiver ! result // a JobResult(jobId, result) object is sent
         }
       }
 
     case GetJobConfig(jobId) =>
-      sender ! jobDao.getJobConfigs.get(jobId).getOrElse(NoSuchJobId)
+      val configs = Await.result(jobDao.getJobConfigs, 60 seconds)
+      sender ! configs.getOrElse(jobId, NoSuchJobId)
 
     case StoreJobConfig(jobId, jobConfig) =>
       jobDao.saveJobConfig(jobId, jobConfig)

job-server/test/spark.jobserver/JobInfoActorSpec.scala
@@ -3,6 +3,8 @@ package spark.jobserver
 import akka.actor.{Props, ActorRef, ActorSystem}
 import akka.testkit.{TestKit, ImplicitSender}
 import org.scalatest.{FunSpecLike, BeforeAndAfter, BeforeAndAfterAll, Matchers}
+import scala.concurrent._
+import scala.concurrent.duration._
 
 import spark.jobserver.io.{JobDAOActor, JobDAO}
 
@@ -43,7 +45,8 @@ with FunSpecLike with Matchers with BeforeAndAfter with BeforeAndAfterAll {
     it("should store a job configuration") {
       actor ! StoreJobConfig(jobId, jobConfig)
       expectMsg(JobConfigStored)
-      dao.getJobConfigs.get(jobId) should be (Some(jobConfig))
+      val jobConfigs = Await.result(dao.getJobConfigs, 60 seconds)
+      jobConfigs.get(jobId) should be (Some(jobConfig))
     }
 
     it("should return a job configuration when the jobId exists") {

job-server-extras/test/spark.jobserver/StreamingJobSpec.scala
@@ -1,8 +1,10 @@
 package spark.jobserver
 
+import scala.concurrent.Await
+
 import com.typesafe.config.ConfigFactory
 import spark.jobserver.context.StreamingContextFactory
-import spark.jobserver.io.{JobInfo, JobDAOActor}
+import spark.jobserver.io.{JobDAOActor, JobInfo}
 
 /**
  * Test for Streaming Jobs.
@@ -47,7 +49,8 @@ class StreamingJobSpec extends JobSpecBase(StreamingJobSpec.getNewSystem) {
         }
       }
       Thread sleep 1000
-      dao.getJobInfo(jobId).get match  {
+      val jobInfo = Await.result(dao.getJobInfo(jobId), 60 seconds)
+      jobInfo.get match  {
         case JobInfo(_, _, _, _, _, None, _) => {  }
         case e => fail("Unexpected JobInfo" + e)
       }

job-server/src/main/resources/application.conf
@@ -1,6 +1,6 @@
 # Settings for safe local mode development
 spark {
-  master = "local[4]"
+  master = "local[*]"
   # spark web UI port
   webUrlPort = 8080
 
@@ -39,7 +39,7 @@ spark {
 
     sqldao {
       # Slick database driver, full classpath
-      slick-driver = scala.slick.driver.H2Driver
+      slick-driver = slick.driver.H2Driver
 
       # JDBC driver, full classpath
       jdbc-driver = org.h2.Driver

job-server/src/spark.jobserver/io/JobDAO.scala
@@ -1,7 +1,9 @@
 package spark.jobserver.io
 
 import com.typesafe.config._
-import org.joda.time.{ Duration, DateTime }
+import org.joda.time.{DateTime, Duration}
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 // Uniquely identifies the jar used to run a job
 case class JarInfo(appName: String, uploadTime: DateTime)
@@ -36,7 +38,7 @@ trait JobDAO {
    *
    * @return
    */
-  def getApps: Map[String, DateTime]
+  def getApps: Future[Map[String, DateTime]]
 
   /**
    * TODO(kelvinchu): Remove this method later when JarManager doesn't use it anymore.
@@ -59,14 +61,14 @@ trait JobDAO {
    *
    * @return
    */
-  def getJobInfo(jobId: String): Option[JobInfo]
+  def getJobInfo(jobId: String): Future[Option[JobInfo]]
 
   /**
    * Return all job ids to their job info.
    *
    * @return
    */
-  def getJobInfos(limit: Int): Seq[JobInfo]
+  def getJobInfos(limit: Int): Future[Seq[JobInfo]]
 
   /**
    * Persist a job configuration along with provided jobId.
@@ -81,12 +83,12 @@ trait JobDAO {
    *
    * @return
    */
-  def getJobConfigs: Map[String, Config]
+  def getJobConfigs: Future[Map[String, Config]]
 
   /**
    * Returns the last upload time for a given app name.
    * @return Some(lastUploadedTime) if the app exists and the list of times is nonempty, None otherwise
    */
   def getLastUploadTime(appName: String): Option[DateTime] =
-    getApps.get(appName)
+    Await.result(getApps, 60 seconds).get(appName)
 }

job-server/src/spark.jobserver/io/JobDAOActor.scala
@@ -1,12 +1,14 @@
 package spark.jobserver.io
 
+import scala.concurrent.Await
+
 import akka.actor.Props
 import com.typesafe.config.Config
 import ooyala.common.akka.InstrumentedActor
 import org.joda.time.DateTime
 import spark.jobserver.io.JobDAOActor.SaveJar
 import spark.jobserver.util.JarUtils
-
+import scala.concurrent.duration._
 object JobDAOActor {
 
   //Requests
@@ -45,7 +47,8 @@ class JobDAOActor(dao:JobDAO) extends InstrumentedActor {
       dao.saveJar(appName, uploadTime, jarBytes)
 
     case GetApps =>
-      sender() ! Apps(dao.getApps)
+      val apps = Await.result(dao.getApps, 60 seconds)
+      sender() ! Apps(apps)
 
     case GetJarPath(appName, uploadTime) =>
       sender() ! JarPath(dao.retrieveJarFile(appName,uploadTime))
@@ -54,13 +57,15 @@ class JobDAOActor(dao:JobDAO) extends InstrumentedActor {
       dao.saveJobInfo(jobInfo)
 
     case GetJobInfos(limit) =>
-      sender() ! JobInfos(dao.getJobInfos(limit))
+      val jobInfo = Await.result(dao.getJobInfos(limit), 60 seconds)
+      sender() ! JobInfos(jobInfo)
 
     case SaveJobConfig(jobId, jobConfig) =>
       dao.saveJobConfig(jobId,jobConfig)
 
     case GetJobConfigs =>
-      sender() ! JobConfigs(dao.getJobConfigs)
+      val jobConfigs = Await.result(dao.getJobConfigs, 60 seconds)
+      sender() ! JobConfigs(jobConfigs)
 
     case GetLastUploadTime(appName) =>
       sender() ! LastUploadTime(dao.getLastUploadTime(appName))

job-server/src/spark.jobserver/io/JobFileDAO.scala
@@ -2,9 +2,12 @@ package spark.jobserver.io
 
 import com.typesafe.config._
 import java.io._
+
 import org.joda.time.DateTime
 import org.slf4j.LoggerFactory
 import scala.collection.mutable
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class JobFileDAO(config: Config) extends JobDAO {
   private val logger = LoggerFactory.getLogger(getClass)
@@ -97,7 +100,7 @@ class JobFileDAO(config: Config) extends JobDAO {
     val outFile = new File(rootDir, createJarName(appName, uploadTime) + ".jar")
     val bos = new BufferedOutputStream(new FileOutputStream(outFile))
     try {
-      logger.debug("Writing {} bytes to file {}", jarBytes.size, outFile.getPath)
+      logger.debug("Writing {} bytes to file {}", jarBytes.length, outFile.getPath)
       bos.write(jarBytes)
       bos.flush()
     } finally {
@@ -126,10 +129,12 @@ class JobFileDAO(config: Config) extends JobDAO {
     }
   }
 
-  def getApps: Map[String, DateTime] = apps.map {
-    case (appName, uploadTimes) =>
-      appName -> uploadTimes.head
+  def getApps: Future[Map[String, DateTime]] = Future {
+    apps.map {
+      case (appName, uploadTimes) =>
+        appName -> uploadTimes.head
     }.toMap
+  }
 
   override def retrieveJarFile(appName: String, uploadTime: DateTime): String =
     new File(rootDir, createJarName(appName, uploadTime) + ".jar").getAbsolutePath
@@ -168,17 +173,20 @@ class JobFileDAO(config: Config) extends JobDAO {
     Some(new DateTime(in.readLong)),
     readError(in))
 
-  override def getJobInfo(jobId: String): Option[JobInfo] = jobs.get(jobId)
+  override def getJobInfo(jobId: String): Future[Option[JobInfo]] = Future {
+    jobs.get(jobId)
+  }
 
-  override def getJobInfos(limit: Int): Seq[JobInfo] =
-    jobs.values.toSeq.sortBy(- _.startTime.getMillis).take(limit)
+  override def getJobInfos(limit: Int): Future[Seq[JobInfo]] = Future {
+    jobs.values.toSeq.sortBy(-_.startTime.getMillis).take(limit)
+  }
 
   override def saveJobConfig(jobId: String, jobConfig: Config) {
     writeJobConfig(jobConfigsOutputStream, jobId, jobConfig)
     configs(jobId) = jobConfig
   }
 
-  override def getJobConfigs: Map[String, Config] = configs.toMap
+  override def getJobConfigs: Future[Map[String, Config]] = Future { configs.toMap }
 
   private def writeJobConfig(out: DataOutputStream, jobId: String, jobConfig: Config) {
     out.writeUTF(jobId)

commit 109210c6786f915130b4d40eb62033bfa4c91469

Fix for issue #522
Fix regression caused by f121807.
Also add missing tests.
Remove Await.result pattern to avoid blocking actor.

job-server/src/spark.jobserver/JobInfoActor.scala
@@ -33,38 +33,34 @@ class JobInfoActor(jobDao: JobDAO, contextSupervisor: ActorRef) extends Instrume
 
   override def wrappedReceive: Receive = {
     case GetJobStatuses(limit) =>
-      sender ! jobDao.getJobInfos(limit.get)
+      val originator = sender
+      jobDao.getJobInfos(limit.get).foreach(originator ! _)
 
     case GetJobStatus(jobId) =>
-      val jobInfo = Await.result(jobDao.getJobInfo(jobId), 60 seconds)
-      val resp = if (jobInfo.nonEmpty) NoSuchJobId else jobInfo.get
-      sender ! resp
+      val originator = sender
+
+      jobDao.getJobInfo(jobId).collect {
+        case Some(jobInfo) => originator ! jobInfo
+        case None          => originator ! NoSuchJobId
+      }
 
     case GetJobResult(jobId) =>
-      breakable {
-        val jobInfo: Option[JobInfo] = Await.result(jobDao.getJobInfo(jobId), 60 seconds)
+      val originator = sender
 
-        if (jobInfo.nonEmpty) {
-          sender ! NoSuchJobId
-          break
-        }
-
-        jobInfo.filter { job => job.isRunning || job.isErroredOut }
-          .foreach { jobInfo =>
-            sender ! jobInfo
-            break
+      jobDao.getJobInfo(jobId).collect {
+        case Some(jobInfo) =>
+          if (jobInfo.isRunning || jobInfo.isErroredOut) {
+            originator ! jobInfo
+          } else {
+            // get the context from jobInfo
+            val context = jobInfo.contextName
+            for {
+              resultActor <- (contextSupervisor ? ContextSupervisor.GetResultActor(context)).mapTo[ActorRef]
+              result <- resultActor ? GetJobResult(jobId) } {
+              originator ! result   // a JobResult(jobId, result) object is sent
+            }
           }
-
-        // get the context from jobInfo
-        val context = jobInfo.get.contextName
-
-        val future = (contextSupervisor ? ContextSupervisor.GetResultActor(context)).mapTo[ActorRef]
-        val resultActor = Await.result(future, 3 seconds)
-
-        val receiver = sender // must capture the sender since callbacks are run in a different thread
-        for (result <- resultActor ? GetJobResult(jobId)) {
-          receiver ! result // a JobResult(jobId, result) object is sent
-        }
+        case None => originator ! NoSuchJobId
       }
 
     case GetJobConfig(jobId) =>

job-server/test/spark.jobserver/JobInfoActorSpec.scala
@@ -3,10 +3,11 @@ package spark.jobserver
 import akka.actor.{Props, ActorRef, ActorSystem}
 import akka.testkit.{TestKit, ImplicitSender}
 import org.scalatest.{FunSpecLike, BeforeAndAfter, BeforeAndAfterAll, Matchers}
+import org.joda.time.DateTime
 import scala.concurrent._
 import scala.concurrent.duration._
 
-import spark.jobserver.io.{JobDAOActor, JobDAO}
+import spark.jobserver.io.{JobDAOActor, JobDAO, JobInfo, JarInfo}
 
 object JobInfoActorSpec {
   val system = ActorSystem("test")
@@ -16,7 +17,7 @@ class JobInfoActorSpec extends TestKit(JobInfoActorSpec.system) with ImplicitSen
 with FunSpecLike with Matchers with BeforeAndAfter with BeforeAndAfterAll {
 
   import com.typesafe.config._
-  import CommonMessages.NoSuchJobId
+  import CommonMessages.{NoSuchJobId, GetJobResult}
   import JobInfoActor._
 
   private val jobId = "jobId"
@@ -59,6 +60,65 @@ with FunSpecLike with Matchers with BeforeAndAfter with BeforeAndAfterAll {
     it("should return error if jobId does not exist") {
       actor ! GetJobConfig(jobId)
       expectMsg(NoSuchJobId)
+    }
+
+    it("should return job info when requested for jobId that exists") {
+      val dt = DateTime.parse("2013-05-29T00Z")
+      val jobInfo = JobInfo("foo", "context", JarInfo("demo", dt), "com.abc.meme", dt, None, None)
+      dao.saveJobInfo(jobInfo)
+      actor ! GetJobStatus("foo")
+      expectMsg(jobInfo)
+    }
+
+    it("should return error if job info is requested for jobId that does not exist") {
+      actor ! GetJobStatus("foo")
+      expectMsg(NoSuchJobId)
+    }
+
+    it("should return list of job infos when requested for job statuses") {
+      val dt1 = DateTime.parse("2013-05-28T00Z")
+      val dt2 = DateTime.parse("2013-05-29T00Z")
+      val jobInfo1 = JobInfo("foo-1", "context", JarInfo("demo", dt1), "com.abc.meme", dt2, None, None)
+      val jobInfo2 = JobInfo("foo-2", "context", JarInfo("demo", dt2), "com.abc.meme", dt2, None, None)
+      dao.saveJobInfo(jobInfo1)
+      dao.saveJobInfo(jobInfo2)
+      actor ! GetJobStatuses(Some(10))
+      expectMsg(Seq[JobInfo](jobInfo1, jobInfo2))
+    }
+
+    it("should return as many number of job infos as requested") {
+      val dt1 = DateTime.parse("2013-05-28T00Z")
+      val dt2 = DateTime.parse("2013-05-29T00Z")
+      val jobInfo1 = JobInfo("foo-1", "context", JarInfo("demo", dt1), "com.abc.meme", dt1, None, None)
+      val jobInfo2 = JobInfo("foo-2", "context", JarInfo("demo", dt2), "com.abc.meme", dt2, None, None)
+      dao.saveJobInfo(jobInfo1)
+      dao.saveJobInfo(jobInfo2)
+      actor ! GetJobStatuses(Some(1))
+      expectMsg(Seq[JobInfo](jobInfo1))
+    }
+
+    it("should return empty list if jobs doest not exist") {
+      actor ! GetJobStatuses(Some(1))
+      expectMsg(Seq.empty)
+    }
+
+    it("should return error if job result is requested for jobId that does not exist") {
+      actor ! GetJobResult("foo")
+      expectMsg(NoSuchJobId)
+    }
+
+    it("should return job info if job result is requested for running or errored out job") {
+      val someError: Option[Throwable] = Some(new Throwable("test-error"))
+      val dt1 = DateTime.parse("2013-05-28T00Z")
+      val dt2 = DateTime.parse("2013-05-29T00Z")
+      val jobInfo1 = JobInfo("foo-1", "context", JarInfo("demo", dt1), "com.abc.meme", dt1, None, None)
+      val jobInfo2 = JobInfo("foo-2", "context", JarInfo("demo", dt2), "com.abc.meme", dt2, None, someError)
+      dao.saveJobInfo(jobInfo1)
+      dao.saveJobInfo(jobInfo2)
+      actor ! GetJobResult("foo-1")
+      expectMsg(jobInfo1)
+      actor ! GetJobResult("foo-2")
+      expectMsg(jobInfo2)
     }
   }
 }