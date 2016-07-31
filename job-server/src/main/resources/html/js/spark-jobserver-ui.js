3f4b08abd43fab20e2c6b0d2e9a5f2e0716dbbe0
TianLang <tianlangstudio@aliyun.com>
2016-07-31 16:37:12 +0800

    show jobs by status (error,running,finished)

--- job-server/src/main/resources/html/js/spark-jobserver-ui.js
+++ job-server/src/main/resources/html/js/spark-jobserver-ui.js
@@ -3,7 +3,7 @@ function showJobs(filter,$tableBody) {
         '/jobs',
         filter,
         function(jobs) {
-            $tableBody.empty();
+            $tableBody.html("");
             var jobsHtml = "";
             $.each(jobs, function(key, job) {
                 jobsHtml += "<tr>";
@@ -28,52 +28,50 @@ function getJobs() {
 }
 
 function getContexts() {
-  $.getJSON(
-    '/contexts',
-    '',
-    function(contexts) {
-      $('#contextsTable tbody').empty();
+    $.getJSON(
+        '/contexts',
+        '',
+        function(contexts) {
+            $('#contextsTable tbody').empty();
 
-      $.each(contexts, function(key, contextName) {
-        var items = [];
-        items.push("<tr><td>" + contextName + "</td></tr>");
-        $('#contextsTable > tbody:last').append(items.join(""));
-      });
-    });
+            $.each(contexts, function(key, contextName) {
+                var items = [];
+                items.push("<tr><td>" + contextName + "</td></tr>");
+                $('#contextsTable > tbody:last').append(items.join(""));
+            });
+        });
 }
 
 function getJars() {
-  $.getJSON(
-    '/jars',
-    '',
-    function(jars) {
-      $('#jarsTable tbody').empty();
+    $.getJSON(
+        '/jars',
+        '',
+        function(jars) {
+            $('#jarsTable tbody').empty();
 
-      $.each(jars, function(jarName, deploymentTime) {
-        var items = [];
-        items.push("<tr>");
-        items.push("<td>" + jarName + "</td>");
-        items.push("<td>" + deploymentTime + "</td>");
-        items.push("</tr>");
-        $('#jarsTable > tbody:last').append(items.join(""));
-      });
-    });
+            $.each(jars, function(jarName, deploymentTime) {
+                var items = [];
+                items.push("<tr>");
+                items.push("<td>" + jarName + "</td>");
+                items.push("<td>" + deploymentTime + "</td>");
+                items.push("</tr>");
+                $('#jarsTable > tbody:last').append(items.join(""));
+            });
+        });
 }
 
-$(document).ready(getJobs());
-$(document).ready(getContexts());
-$(document).ready(getJars());
 
 $(function () {
-  $('#navTabs a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-    var target = $(e.target).attr("href");
+    $('#navTabs a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+        var target = $(e.target).attr("href");
 
-    if (target == '#jobs') {
-      getJobs();
-    } else if (target == "#contexts") {
-      getContexts();
-    } else {
-      getJars();
-    }
-  })
+        if (target == '#jobs') {
+            getJobs();
+        } else if (target == "#contexts") {
+            getContexts();
+        } else {
+            getJars();
+        }
+    })
+    getJobs();
 });
