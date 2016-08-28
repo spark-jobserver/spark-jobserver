8667393c84799f43ec44f233f6430da066643f89
f1yegor <f1yegor@gmail.com>
2016-08-28 18:48:33 +0200

    remove scala 2.10 build

--- project/Build.scala
+++ project/Build.scala
@@ -233,7 +233,6 @@ object JobServerBuild extends Build {
   lazy val commonSettings = Defaults.coreDefaultSettings ++ dirSettings ++ implicitlySettings ++ Seq(
     organization := "spark.jobserver",
     crossPaths   := true,
-    crossScalaVersions := Seq("2.10.6","2.11.8"),
     scalaVersion := sys.env.getOrElse("SCALA_VERSION", "2.11.8"),
     publishTo    := Some(Resolver.file("Unused repo", file("target/unusedrepo"))),
     // scalastyleFailOnError := true,
