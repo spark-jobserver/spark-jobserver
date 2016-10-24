9084053cf6b09e02ce8c148af7745f8182cc0755
Wang Lei <lei.wang@kongming-inc.com>
2016-10-24 19:16:30 +0800

    add mesos cluster mode support

--- job-server/src/main/scala/spark/jobserver/io/JobDAOActor.scala
+++ job-server/src/main/scala/spark/jobserver/io/JobDAOActor.scala
@@ -24,6 +24,9 @@ object JobDAOActor {
   case class GetBinaryPath(appName: String,
                            binaryType: BinaryType,
                            uploadTime: DateTime) extends JobDAORequest
+  case class GetBinaryContent(appName: String,
+                              binaryType: BinaryType,
+                              uploadTime: DateTime) extends JobDAORequest
 
   case class SaveJobInfo(jobInfo: JobInfo) extends JobDAORequest
   case class GetJobInfos(limit: Int) extends JobDAORequest
@@ -37,6 +40,7 @@ object JobDAOActor {
   sealed trait JobDAOResponse
   case class Apps(apps: Map[String, (BinaryType, DateTime)]) extends JobDAOResponse
   case class BinaryPath(binPath: String) extends JobDAOResponse
+  case class BinaryContent(content: Array[Byte]) extends JobDAOResponse
   case class JobInfos(jobInfos: Seq[JobInfo]) extends JobDAOResponse
   case class JobConfigs(jobConfigs: Map[String, Config]) extends JobDAOResponse
   case class LastUploadTimeAndType(uploadTimeAndType: Option[(DateTime, BinaryType)]) extends JobDAOResponse
@@ -79,5 +83,8 @@ class JobDAOActor(dao:JobDAO) extends InstrumentedActor {
 
     case GetLastUploadTimeAndType(appName) =>
       sender() ! LastUploadTimeAndType(dao.getLastUploadTimeAndType(appName))
+
+    case GetBinaryContent(appName, binaryType, uploadTime) =>
+      sender() ! BinaryContent(dao.getBinaryContent(appName, binaryType, uploadTime))
   }
 }
