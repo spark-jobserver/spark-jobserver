9195bee648c04e03997833680e91508a167e1e98
Addison Higham <ahigham@instructure.com>
2016-10-31 08:46:10 -0600

    update spark install loc

--- ci/install-spark.sh
+++ ci/install-spark.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -o /tmp/spark.tgz http://apache.mirror.anlx.net/spark/spark-2.0.0/spark-2.0.0-bin-hadoop2.7.tgz
+curl -L -o /tmp/spark.tgz http://d3kbcqa49mib13.cloudfront.net/spark-2.0.1-bin-hadoop2.7.tgz
 tar -xvzf /tmp/spark.tgz -C /tmp
