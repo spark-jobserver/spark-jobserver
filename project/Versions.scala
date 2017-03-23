dd0a139a80f6010ff283797e5071c57d1d378a50
Arun Allamsetty <arun@instructure.com>
2017-03-23 15:49:02 +0530

    Upgrade spark to 2.1.
    - Fix existing compile errors.
    - Sort variables in Versions.scala for readability.
    - Make Versions.scala the one-stop for all version resolution rather
    than re-resolving them in Dependencies.scala.
    - (Amend) Return -1 when an exception is thrown in JStreamingTestJob.
    - (Amend) Upgrading netty-io version to be the same as Spark 2.1. More
    of a shot in the dark as I cannot reproduce the error in
    JobCassandraDAOSpec.

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,25 +1,28 @@
+import scala.util.Properties.isJavaAtLeast
+
 object Versions {
-  val jdkVersion = scala.util.Properties.isJavaAtLeast("1.8")
-  lazy val typeSafeConfig = if(jdkVersion) "1.3.0" else "1.2.1"
-  lazy val metrics = "2.2.0"
-  lazy val jodaTime = "2.9.3"
-  lazy val jodaConvert = "1.8.1"
+  lazy val spark = sys.env.getOrElse("SPARK_VERSION", "2.1.0")
+
   lazy val akka = "2.4.9"
-  lazy val spray = "1.3.3"
-  lazy val sprayJson = "1.3.2"
-  lazy val spark = "2.0.1"
-  lazy val mesos = "1.0.0-2.0.89.ubuntu1404"
-  lazy val netty =  "4.0.29.Final"
-  lazy val slick = "3.1.1"
-  lazy val h2 = "1.3.176"
-  lazy val postgres = "9.4.1209"
   lazy val cassandra = "3.0.3"
+  lazy val cassandraUnit = "2.2.2.1"
   lazy val commons = "1.4"
   lazy val flyway = "3.2.1"
+  lazy val h2 = "1.3.176"
+  lazy val java = sys.env.getOrElse("JAVA_VERSION", "8-jdk")
+  lazy val jodaConvert = "1.8.1"
+  lazy val jodaTime = "2.9.3"
   lazy val logback = "1.0.7"
+  lazy val mesos = sys.env.getOrElse("MESOS_VERSION", "1.0.0-2.0.89.ubuntu1404")
+  lazy val metrics = "2.2.0"
+  lazy val netty =  "4.0.42.Final"
+  lazy val postgres = "9.4.1209"
+  lazy val py4j = "0.10.3"
   lazy val scalaTest = "2.2.6"
   lazy val scalatic = "2.2.6"
   lazy val shiro = "1.2.4"
-  lazy val cassandraUnit = "2.2.2.1"
-  lazy val py4j = "0.10.3"
+  lazy val slick = "3.1.1"
+  lazy val spray = "1.3.3"
+  lazy val sprayJson = "1.3.2"
+  lazy val typeSafeConfig = if (isJavaAtLeast("1.8")) "1.3.0" else "1.2.1"
 }
