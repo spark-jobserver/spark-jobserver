4ab3192aff4b344350c2dd6bf3221ea27bd8b57d
Behroz Sikander <behroz.sikander@sap.com>
2017-05-31 11:25:52 +0200

    Add necessary files to build sjs with xmake.
    Change-Id: Ifa3fa7857e81a15a34bc62a80109e71e509c5696

---
+++ cfg/export.ads
@@ -0,0 +1,8 @@
+artifacts builderVersion:"1.1",{
+
+	group "com.sap.sparkjobserver",{
+		artifact "sparkjobserver-jar", {
+			file "$componentroot/job-server/target/scala-2.10/spark-job-server.jar"
+		}
+	}
+}
