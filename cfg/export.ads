4385816733876fe4b8b7a2080f2c5813b3504104
Behroz Sikander <behroz.sikander@sap.com>
2017-06-01 20:19:55 +0200

    Move the sbt cache from the default directory to project folder.
    * During milestone build, the jars should be downloaded again
    each time to ensure proper release.
    * Upgrade the version of release.
    
    Change-Id: Id2940bc276cdb2d751d94eb9d6f2a3975b9cc0ac

--- cfg/export.ads
+++ cfg/export.ads
@@ -1,6 +1,6 @@
 artifacts builderVersion: "1.1", {
-  group "com.sap.sparkjobserver", {
-    artifact "sparkjobserver-jar", {
+  group "spark.jobserver", {
+    artifact "sparkjobserver", {
       file "$componentroot/job-server/target/scala-2.10/spark-job-server.jar"
     }
   }
