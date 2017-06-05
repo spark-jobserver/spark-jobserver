faf13b2409a805f0de1ef85830f63be11a4e42c7
Junji Shimagaki <junji.shimagaki@sony.com>
2017-06-05 11:08:20 +0900

    Remove `spark-jobserver.pid` if such process does not exist
    When `bin/server-stop.sh` is executed but not corresponding PID exists,
    the dead PID file exists forever, and that blocks all next `server-stop.sh`
    executions from successfully terminating the SJS process.
    
    This commit fix the issue by removing the PID file in case such a PID file
    remains after the SJS termination attempt.

--- bin/server_stop.sh
+++ bin/server_stop.sh
@@ -18,15 +18,17 @@ else
 fi
 
 pidFilePath=$appdir/$PIDFILE
+PID="$(cat "$pidFilePath")"
 
 if [ ! -f "$pidFilePath" ] || ! kill -0 "$(cat "$pidFilePath")"; then
    echo 'Job server not running'
 else
   echo 'Stopping job server...'
-  PID="$(cat "$pidFilePath")"
   "$(dirname "$0")"/kill-process-tree.sh 15 $PID && rm "$pidFilePath"
   echo '...job server stopped'
 fi
 
-
-
+if [ -f "$pidFilePath" ]; then
+  echo "Removing PID file, cause there is not such process ${PID}."
+  rm "$pidFilePath"
+fi
