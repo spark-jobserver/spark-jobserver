aecfd8e21b12103424843df0e738bb317804de46
Junji Shimagaki <junji.shimagaki@sony.com>
2017-06-05 15:25:14 +0900

    fix(bin): make sure that PID is valid and process ended by server_stop.sh
    * Remove the PID file in case the mentioned process does not exist
    * Make sure that the process terminated after kill-process-tree.sh

--- bin/server_stop.sh
+++ bin/server_stop.sh
@@ -19,14 +19,22 @@ fi
 
 pidFilePath=$appdir/$PIDFILE
 
-if [ ! -f "$pidFilePath" ] || ! kill -0 "$(cat "$pidFilePath")"; then
-   echo 'Job server not running'
+if [ ! -f "$pidFilePath" ]; then
+  echo 'Job server not running'
 else
-  echo 'Stopping job server...'
   PID="$(cat "$pidFilePath")"
-  "$(dirname "$0")"/kill-process-tree.sh 15 $PID && rm "$pidFilePath"
-  echo '...job server stopped'
+  if ! kill -0 $PID; then
+    echo "PID file exists but the process $PID does not exist. Removing $pidFilePath"
+    rm "$pidFilePath"
+  else
+    echo 'Stopping job server...'
+    "$(dirname "$0")"/kill-process-tree.sh 15 $PID
+    if ! kill -0 $PID 2> /dev/null ; then
+      echo '...job server stopped'
+      rm "$pidFilePath"
+    else
+      echo '...?? job server is still running'
+    fi
+  fi
 fi
-
-
 
