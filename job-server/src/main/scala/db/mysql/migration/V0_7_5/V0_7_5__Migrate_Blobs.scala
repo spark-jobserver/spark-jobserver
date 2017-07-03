94b5c055e901c3fcbfa58e32186a6d5e28b28cc2
Matthias Sohn <matthias.sohn@sap.com>
2017-07-03 16:31:29 +0200

    fix(job-server): JobSQLDAO: Move blobs to a separate table (#876)
    * Separate the BINARIES table into BINARIES and BINARIES_CONTENTS, so
    that only the BINARIES_CONTENTS table contains the "BINARY" (Blob)
    column. Both tables can be joined on the same BIN_ID.
    
    * Optimize the JobDAO.getLastUploadTimeAndType(...) method - make it
    abstract and implement an optimized version in the JobFileDAO and
    JobSqlDAO.
    
    This change is based on previous work done by Michał Januszewski in pull
    request #886.

---
+++ job-server/src/main/scala/db/mysql/migration/V0_7_5/V0_7_5__Migrate_Blobs.scala
@@ -0,0 +1,38 @@
+package db.mysql.migration.V0_7_5
+
+import java.sql.Blob
+import java.sql.Connection
+import javax.sql.rowset.serial.SerialBlob
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.util.control.NonFatal
+
+import db.migration.V0_7_5.Migration
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration
+import org.slf4j.LoggerFactory
+import slick.dbio.DBIO
+import slick.dbio.Effect
+import slick.dbio.NoStream
+import slick.driver.MySQLDriver.api.actionBasedSQLInterpolation
+import slick.jdbc.GetResult
+import slick.jdbc.PositionedParameters
+import slick.jdbc.SetParameter
+import slick.profile.SqlAction
+import spark.jobserver.slick.unmanaged.UnmanagedDatabase
+
+class V0_7_5__Migrate_Blobs extends Migration {
+  val logger = LoggerFactory.getLogger(getClass)
+
+  protected def insertBlob(id: Int, blob: SerialBlob): SqlAction[Int, NoStream, Effect] = {
+    sqlu"""INSERT INTO `BINARIES_CONTENTS` (`BIN_ID`, `BINARY`) VALUES (${id}, ${blob})"""
+  }
+  val createContentsTable = sqlu"""CREATE TABLE `BINARIES_CONTENTS` (
+    `BIN_ID`  SERIAL  NOT NULL PRIMARY KEY,
+    `BINARY`  LONGBLOB
+  );"""
+  val getBinaryContents = sql"""SELECT `BIN_ID`, `BINARY` FROM `BINARIES`""".as[BinaryContent]
+  val dropColumn = sqlu"""ALTER TABLE `BINARIES` DROP COLUMN `BINARY`"""
+}
+
