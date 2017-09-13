c2082d7a9110c91b97df19abf384db5146408507
Sascha Wolke <sascha.wolke@campus.tu-berlin.de>
2017-09-13 21:22:22 +0200

    cleanup(config): remove default broadcast factory
    TorrentBroadcastFactory is already default since Spark 1.2

--- job-server-api/src/main/scala/spark/jobserver/util/SparkJobUtils.scala
+++ job-server-api/src/main/scala/spark/jobserver/util/SparkJobUtils.scala
@@ -82,11 +82,6 @@ object SparkJobUtils {
     // Set the Jetty port to 0 to find a random port
     conf.set("spark.ui.port", "0")
 
-    // Set spark broadcast factory in yarn-client mode
-    if (sparkMaster == "yarn-client") {
-      conf.set("spark.broadcast.factory", config.getString("spark.jobserver.yarn-broadcast-factory"))
-    }
-
     // Set number of akka threads
     // TODO: need to figure out how many extra threads spark needs, besides the job threads
     conf.set("spark.akka.threads", (getMaxRunningJobs(config) + 4).toString)
