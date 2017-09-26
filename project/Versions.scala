18ec021cc1df00b6a356f1ca86ba619740297067
Noorul Islam K M <noorul@noorul.com>
2017-09-26 17:38:19 +0530

    fix(core): Update spark version to 2.2.0 (#845)
    * Explicitly add hadoop-client dependency
    * Update netty version
    * Update C* driver version
    * Remove metastore_db lock across tests

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,13 +1,15 @@
 import scala.util.Properties.isJavaAtLeast
 
 object Versions {
-  lazy val spark = sys.env.getOrElse("SPARK_VERSION", "2.1.0")
+  lazy val spark = sys.env.getOrElse("SPARK_VERSION", "2.2.0")
 
   lazy val akka = "2.4.9"
-  lazy val cassandra = "3.0.3"
+  lazy val cassandra = "3.3.0"
   lazy val cassandraUnit = "2.2.2.1"
   lazy val commons = "1.4"
+  lazy val derby = "10.12.1.1"
   lazy val flyway = "3.2.1"
+  lazy val hadoop = "2.7.3"
   lazy val h2 = "1.3.176"
   lazy val java = sys.env.getOrElse("JAVA_VERSION", "8-jdk")
   lazy val jodaConvert = "1.8.1"
@@ -15,7 +17,7 @@ object Versions {
   lazy val logback = "1.0.7"
   lazy val mesos = sys.env.getOrElse("MESOS_VERSION", "1.0.0-2.0.89.ubuntu1404")
   lazy val metrics = "2.2.0"
-  lazy val netty = "4.0.42.Final"
+  lazy val netty = "4.0.44.Final"
   lazy val postgres = "9.4.1209"
   lazy val mysql = "5.1.42"
   lazy val py4j = "0.10.4"
