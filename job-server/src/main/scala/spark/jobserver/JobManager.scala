1fec647c5035df2f5f5574b4492b6accc5ed61e1
Peter Kotula <justcoonjust@yahoo.com>
2018-03-06 21:13:37 +0100

    #971, #995 - log message fix

--- job-server/src/main/scala/spark/jobserver/JobManager.scala
+++ job-server/src/main/scala/spark/jobserver/JobManager.scala
@@ -56,7 +56,7 @@ object JobManager {
       systemConfig.resolve()
     }
 
-    logger.info("Starting JobManager named " + managerName + " with config {}",
+    logger.info("Starting JobManager named {} with config {}", managerName, 
       config.getConfig("spark").root.render())
 
     val system = makeSystem(config)
