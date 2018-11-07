50674aeda6eefe46fb7acbcf10b6962de7e43aa8
Valentina Glagoleva <valentina.glagoleva@sap.com>
2018-11-07 10:03:19 +0100

    feat(jobserver): Add HDFS implementation for BinaryDAO
    Add BinaryDAO implementation for HDFS (with tests and validation of config).
    
    Change-Id: Idd674b2328dc0c93fc92464fcb82f79edebff1f1

---
+++ job-server/src/main/scala/spark/jobserver/io/HdfsBinaryDAO.scala
@@ -0,0 +1,53 @@
+package spark.jobserver.io
+
+import java.nio.file.FileAlreadyExistsException
+
+import com.typesafe.config.Config
+import org.apache.commons.io.IOUtils.toByteArray
+import org.slf4j.LoggerFactory
+import spark.jobserver.util.{HadoopFSFacade, Utils}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+/**
+  * Data access object for retrieving/persisting binary data on HDFS.
+  * @param config config of jobserver
+  */
+class HdfsBinaryDAO(config: Config) extends BinaryDAO {
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  private val binaryBasePath =
+    config.getString("spark.jobserver.combineddao.binarydao.dir").stripPrefix("/").stripSuffix("/")
+  private val hdfsFacade = new HadoopFSFacade()
+
+  override def validateConfig(config: Config): Boolean = {
+    config.hasPath("spark.jobserver.combineddao.binarydao.dir")
+  }
+
+  override def save(id: String, binaryBytes: Array[Byte]): Future[Boolean] = {
+    Future {
+      hdfsFacade.save(extendPath(id), binaryBytes, skipIfExists = true)
+    }
+  }
+
+  override def delete(id: String): Future[Boolean] = {
+    Future {
+      hdfsFacade.delete(extendPath(id))
+    }
+  }
+
+  override def get(id: String): Future[Option[Array[Byte]]] = {
+    Future {
+      hdfsFacade.get(extendPath(id)) match {
+        case Some(bytesArray) =>
+          Some(Utils.usingResource(bytesArray)(toByteArray))
+        case None =>
+          logger.error(s"Failed to get a file $id from HDFS.")
+          None
+      }
+    }
+  }
+
+  private def extendPath(id: String): String = s"$binaryBasePath/$id"
+}
--- job-server/src/main/scala/spark/jobserver/util/HadoopFSFacade.scala
+++ job-server/src/main/scala/spark/jobserver/util/HadoopFSFacade.scala
@@ -1,11 +1,13 @@
 package spark.jobserver.util
 
-import java.io.{IOException, InputStreamReader, Reader}
+import java.io.{InputStreamReader, Reader}
 import java.net.URI
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.slf4j.LoggerFactory
+
+import scala.util.control.NonFatal
 
 /**
   * Facade to wrap hadoop filesystem operations.
@@ -25,43 +27,90 @@ class HadoopFSFacade(hadoopConf: Configuration = new Configuration()) {
     */
   def isFile(filePath: String): Option[Boolean] = {
     try {
-      val path = new Path(filePath)
-      val uri = getURI(path)
-      val fs = FileSystem.get(uri, hadoopConf)
-      Some(fs.isFile(path))
+      val fs = FileSystem.get(getPathURI(filePath), hadoopConf)
+      Some(fs.isFile(new Path(filePath)))
     } catch {
-      case e @ (_: IllegalArgumentException | _: IOException) =>
+      case NonFatal(e) =>
         logger.error(e.getMessage)
         None
     }
   }
 
   /**
-    * This function can read config file from all the supported protocols by HDFS
-    * @param filePath absolute path of config file with any supported protocol file/hdfs/s3
+    * This function can read file from all the supported protocols by HDFS
+    * @param filePath absolute path of the file with any supported protocol file/hdfs/s3
     * @return file stream
     */
   def get(filePath: String): Option[Reader] = {
     try {
-      val fs = FileSystem.get(getURI(filePath), hadoopConf)
+      val fs = FileSystem.get(getPathURI(filePath), hadoopConf)
       val stream = fs.open(new Path(filePath))
       Some(new InputStreamReader(stream))
     } catch {
-      case e @ (_: IllegalArgumentException | _ : IOException) =>
+      case NonFatal(e) =>
         logger.error(e.getMessage)
         None
     }
   }
 
-  private def getURI(filePath: Path): URI = {
-    Option(filePath.toUri().getScheme()) match {
-      case None => new URI(s"file:${filePath.toString}")
-      case _ => filePath.toUri()
+  /**
+    * This function can save given bytes under the given file path
+    * @param filePath file path where the file should be saved
+    * @param bytes bytes to be saved under the given filePath
+    * @param skipIfExists do nothing if file with given name already exists, return success
+    * @return true if operation succeeds, else false
+    */
+  def save(filePath: String, bytes: Array[Byte], skipIfExists: Boolean = false): Boolean = {
+    try {
+      val path = new Path(filePath)
+      val fs = FileSystem.get(path.toUri, hadoopConf)
+      if (skipIfExists && fs.exists(path)) {
+        logger.info(s"Skip saving the file: $filePath already exists on HDFS.")
+      } else {
+        val out = fs.create(path)
+        Utils.usingResource(out) {
+          out => out.write(bytes)
+        }
+      }
+      true
+    } catch {
+      case NonFatal(e) =>
+        logger.error(e.getMessage)
+        false
     }
   }
 
-  private def getURI(filePath: String): URI = {
-    val path = new Path(filePath)
-    getURI(path)
+  /**
+    * This function can delete file(s) under the given file path
+    * @param filePath path of the file to be deleted
+    * @param recursive if set to true will delete all subfolders
+    * @return true if operation succeeds, else false
+    */
+  def delete(filePath: String, recursive: Boolean = false): Boolean = {
+    try {
+      val fs = FileSystem.get(getPathURI(filePath), hadoopConf)
+      fs.delete(new Path(filePath), recursive)
+    } catch {
+      case NonFatal(e) =>
+        logger.error(e.getMessage)
+        false
+    }
+  }
+
+  /**
+    * This function returns a URI of the file path. If there is no scheme specified
+    * it applies default scheme (defaults to "file:").
+    * Example: /tmp/test.txt -> file:/tmp/test.txt
+    *          hdfs:///tmp/test.txt -> hdfs:///tmp/test.txt (already has schema)
+    * @param filePath path of the file
+    * @return URI of the path
+    */
+  def getPathURI(filePath: String, defaultSchema: String = "file"): URI = {
+    val path = new Path(filePath).toUri
+    Option(path.getScheme) match {
+      case None => new URI(s"$defaultSchema:$filePath")
+      case _ => path
+    }
   }
 }
+
