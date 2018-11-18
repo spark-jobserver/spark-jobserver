9594dc38b29a66e231fc646529ba787ff738cf05
sgautrin <sgautrin@users.noreply.github.com>
2018-11-18 09:30:42 +0100

    fix(job-server): use 24h format for cached binary file name (#1130)

--- job-server/src/test/scala/spark/jobserver/io/FileCacherSpec.scala
+++ job-server/src/test/scala/spark/jobserver/io/FileCacherSpec.scala
@@ -11,8 +11,8 @@ class FileCacherSpec extends FileCacher with FunSpecLike with Matchers {
   override val rootDirFile: File = new File(rootDir)
 
   it("produces binary name") {
-    val appName = createBinaryName("job", BinaryType.Jar, DateTime.parse("2016-10-10T01:00:00Z"))
-    appName should be("job-20161010_010000_000.jar")
+    val appName = createBinaryName("job", BinaryType.Jar, DateTime.parse("2016-10-10T13:00:00Z"))
+    appName should be("job-20161010_130000_000.jar")
   }
 
   it("clean cache binaries") {
