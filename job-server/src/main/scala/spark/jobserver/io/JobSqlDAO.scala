cd916f17a68a5646974a776f21e43eb07b4279eb
Ecaterina Andreev <ecaterina.andreev@sap.com>
2018-12-05 16:18:54 +0100

    fix(jobserver): keeping unused binaries
    In some special cases unused binaries were kept in the DB.
    For example: Consider the following 3 binaries in the DB:
    1) Name - "appA", Hash - 123
    2) Name - "appA", Hash - 456
    3) Name - "appB", Hash - 123
    If someone would try to delete binary with the name "appA", the names
    would be deleted, but not the binaries themselves. It means that binary
    with hash "456" would still be available in the "binary_contents" table
    and there would be no possibility to delete it.
    
    Other scenarios:
    1- If user deletes appA then app should be deleted but binary
     in binary contents table should remain.
     appA hash1
     appB hash1
     appC hash2
    
    2- If user delets appA then both hashes/bin should be cleaned
    from binary contents table
     appA hash1
     appA hash2
    
    3- If user deletes appA then appB and hash/binary for appB should
    stay.
     appA hash1
     appB hash2
    
    Change-Id: I14a6f6070d78bf45debf2b3d7f4857ad647e01c6

--- job-server/src/main/scala/spark/jobserver/io/JobSqlDAO.scala
+++ job-server/src/main/scala/spark/jobserver/io/JobSqlDAO.scala
@@ -127,20 +127,33 @@ class JobSqlDAO(config: Config, sqlCommon: SqlCommon) extends JobDAO with FileCa
   }
 
   private def deleteBinaryInfo(appName: String): Future[Int] = {
-    val deleteBinary = sqlCommon.binaries.filter(_.appName === appName)
-    val hashUsed = sqlCommon.binaries
-    .filter(_.binHash in deleteBinary.map(_.binHash)).filter(_.appName =!= appName)
-    val deleteBinariesContents = binariesContents.filter(_.binHash in deleteBinary.map(_.binHash))
+    val candidateDeleteBinaries = sqlCommon.binaries.filter(_.appName === appName)
+    val allBinariesUsingCandidateHash = sqlCommon.binaries.filter(
+      _.binHash in candidateDeleteBinaries.map(_.binHash))
+
     val dbAction = (for {
-      _ <- hashUsed.result.headOption.flatMap{
-        case None =>
-          deleteBinariesContents.delete
-        case Some(bc) =>
-          DBIO.successful(None) // no-op
-      }
-      b <- deleteBinary.delete
+      candidateBinaries <- candidateDeleteBinaries.result
+      allBinariesUsingHash <- allBinariesUsingCandidateHash.result
+      hashesToDelete = findAllHashesToDelete(appName, allBinariesUsingHash, candidateBinaries)
+      _ <- binariesContents.filter(_.binHash.inSet(hashesToDelete)).delete
+      b <- candidateDeleteBinaries.delete
     } yield b).transactionally
+
     sqlCommon.db.run(dbAction).recover(logDeleteErrors)
+  }
+
+  private def findAllHashesToDelete(appName: String,
+                                    allBinariesUsingHash: Seq[(Int, String, String, Timestamp, Array[Byte])],
+                                    candidateBinaries: Seq[(Int, String, String, Timestamp, Array[Byte])]):
+                                      Seq[Array[Byte]] = {
+    val hashOfOtherApps = allBinariesUsingHash
+                            .filter(_._2 != appName) // filter other apps
+                            .map(_._5) // hash
+                            .map(BinaryDAO.hashBytesToString(_))
+    val deleteCandidates = candidateBinaries.map( _._5).map(BinaryDAO.hashBytesToString(_))
+    deleteCandidates
+      .filterNot(hashOfOtherApps.contains(_))
+      .map(BinaryDAO.hashStringToBytes(_))
   }
 
   // Fetch the binary file from the database
