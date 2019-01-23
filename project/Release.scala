87524f8d060ef9ec43c489652d2b9366f601d8c6
Ecaterina Andreev <ecaterina.andreev@sap.com>
2019-01-23 17:19:11 +0100

    Revert misc(all): Update sbt to 1.1.6 and update sbt plugins
    Change-Id: Ic407e24bbc4704c1c72d3dbb8e4371419f302e86

--- project/Release.scala
+++ project/Release.scala
@@ -1,9 +1,32 @@
+import sbt._
+import Keys._
 import sbtrelease.ReleasePlugin.autoImport._
 import ReleaseTransformations._
 
-object Release {
+object JobServerRelease {
 
-  lazy val settings = Seq(
+  import ls.Plugin._
+  import LsKeys._
+
+  lazy val implicitlySettings = {
+    lsSettings ++ Seq(
+      homepage := Some(url("https://github.com/spark-jobserver/spark-jobserver")),
+      tags in lsync := Seq("spark", "akka", "rest"),
+      description in lsync := "REST job server for Apache Spark",
+      externalResolvers in lsync := Seq("Job Server Bintray" at "http://dl.bintray.com/spark-jobserver/maven"),
+      ghUser in lsync := Some("spark-jobserver"),
+      ghRepo in lsync := Some("spark-jobserver"),
+      ghBranch in lsync := Some("master")
+    )
+  }
+
+  val syncWithLs = (ref: ProjectRef) => ReleaseStep(
+    check = releaseStepTaskAggregated(LsKeys.writeVersion in lsync in ref),
+    action = releaseStepTaskAggregated(lsync in lsync in ref)
+  )
+
+  lazy val ourReleaseSettings = Seq(
+
     releaseProcess := Seq(
       checkSnapshotDependencies,
       runClean,
@@ -13,6 +36,8 @@ object Release {
       commitReleaseVersion,
       tagRelease,
       publishArtifacts,
+      // lsync seems broken, always returning: Error synchronizing project libraries Unexpected response status: 404
+      // syncWithLs(thisProjectRef.value),
       setNextVersion,
       commitNextVersion,
       pushChanges
