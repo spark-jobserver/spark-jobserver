61061c9f5e10d98cede7cc6dfaf2a9fa23d6e081
Robin Wolters <robin.wolters@sap.com>
2019-01-29 09:17:47 +0100

    feat(jobserver): Implement ZookeeperDao
    * Implement get/save for contexts, jobs, jobconfigs
    * Implement hashCode and equals for ContextInfo for proper object comparison
    * Implement JsonProtocols for contexts and jobs
    * Add further tests to ensure unaltered behavior
    
    Change-Id: I3b5420a61b18f2eb144f1f7715001df3d676e64d

--- job-server/src/main/scala/spark/jobserver/io/zookeeper/MetaDataZookeeperDAO.scala
+++ job-server/src/main/scala/spark/jobserver/io/zookeeper/MetaDataZookeeperDAO.scala
@@ -1,6 +1,8 @@
 package spark.jobserver.io.zookeeper
 
 import com.typesafe.config.Config
+import com.typesafe.config.ConfigRenderOptions
+import com.typesafe.config.ConfigFactory
 import org.joda.time.DateTime
 import org.slf4j.LoggerFactory
 import spark.jobserver.JobServer.InvalidConfiguration
@@ -11,8 +13,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object MetaDataZookeeperDAO {
-  private val baseDirPath = "spark.jobserver.zookeeper.dir"
-  private val connectionStringPath = "spark.jobserver.zookeeper.connection-string"
+  private val baseDirPath = "spark.jobserver.zookeeperdao.dir"
+  private val connectionStringPath = "spark.jobserver.zookeeperdao.connection-string"
   private val binariesDir = "/binaries"
   private val contextsDir = "/contexts"
   private val jobsDir = "/jobs"
@@ -23,6 +25,10 @@ class MetaDataZookeeperDAO(config: Config) extends MetaDataDAO {
   import JsonProtocols._
 
   private val logger = LoggerFactory.getLogger(getClass)
+
+  /*
+   * Configuration
+   */
 
   if (!config.hasPath(baseDirPath)) {
     throw new InvalidConfiguration(
@@ -39,45 +45,167 @@ class MetaDataZookeeperDAO(config: Config) extends MetaDataDAO {
   private val connectionString = config.getString(connectionStringPath)
   private val zookeeperUtils = new ZookeeperUtils(connectionString, config.getString(baseDirPath))
 
+  /*
+   * Contexts
+   */
+
   override def saveContext(contextInfo: ContextInfo): Future[Boolean] = {
-    throw new NotImplementedError()
+    Future {
+      val id = contextInfo.id
+      logger.debug(s"Saving or updating context under: $contextsDir/$id")
+      zookeeperUtils.write(contextInfo, s"$contextsDir/$id")
+    }
   }
 
   override def getContext(id: String): Future[Option[ContextInfo]] = {
-    throw new NotImplementedError()
+    Future {
+      logger.debug(s"Reading context from $contextsDir/$id")
+      zookeeperUtils.read[ContextInfo](s"$contextsDir/$id") match {
+        case Some(contextInfo) =>
+          Some(contextInfo)
+        case None =>
+          logger.info(s"Didn't find any context information for the path: $contextsDir/$id")
+          None
+      }
+    }
   }
 
   override def getContextByName(name: String): Future[Option[ContextInfo]] = {
-    throw new NotImplementedError()
+    Future {
+      zookeeperUtils.list(contextsDir)
+        .flatMap(id => zookeeperUtils.read[ContextInfo](s"$contextsDir/$id"))
+        .filter(_.name == name)
+        .sortBy(_.startTime.getMillis)
+        .lastOption
+    }
   }
 
   override def getContexts(limit: Option[Int], statuses: Option[Seq[String]]): Future[Seq[ContextInfo]] = {
-    Future{Seq.empty[ContextInfo]}
+    Future {
+      lazy val allContexts = zookeeperUtils.list(contextsDir)
+        .flatMap(id => zookeeperUtils.read[ContextInfo](s"$contextsDir/$id"))
+      lazy val filteredContexts = statuses match {
+        case None => allContexts
+        case Some(allowed) => allContexts.filter(c => allowed.contains(c.state))
+      }
+      lazy val sortedContexts = filteredContexts.sortBy(_.startTime.getMillis)(Ordering[Long].reverse)
+      limit match {
+        case None => sortedContexts
+        case Some(l) => sortedContexts.take(l)
+      }
+    }
   }
 
+  /*
+   * Jobs
+   */
+
   override def saveJob(jobInfo: JobInfo): Future[Boolean] = {
-    throw new NotImplementedError()
+    Future {
+      val jobId = jobInfo.jobId
+      logger.debug(s"Saving or updating job under: $jobsDir/$jobId")
+      zookeeperUtils.write(jobInfo, s"$jobsDir/$jobId")
+    }
   }
 
   override def getJob(id: String): Future[Option[JobInfo]] = {
-    throw new NotImplementedError()
+    Future {
+      readJobInfo(id).flatMap(j => refreshBinary(j))
+    }
   }
 
   override def getJobs(limit: Int, status: Option[String]): Future[Seq[JobInfo]] = {
-    throw new NotImplementedError()
+    Future {
+      lazy val allJobs = zookeeperUtils.list(jobsDir)
+        .flatMap(id => readJobInfo(id))
+      lazy val filteredJobs =
+        status match {
+          case None => allJobs
+          case Some(allowed) => allJobs.filter(_.state == allowed)
+        }
+      filteredJobs.sortBy(_.startTime.getMillis)(Ordering[Long].reverse)
+        .take(limit)
+        .flatMap(j => refreshBinary(j))
+    }
   }
 
   override def getJobsByContextId(contextId: String, statuses: Option[Seq[String]]): Future[Seq[JobInfo]] = {
-    throw new NotImplementedError()
+    Future {
+      lazy val allJobs = zookeeperUtils.list(jobsDir)
+        .flatMap(id => readJobInfo(id))
+        .filter(_.contextId == contextId)
+      lazy val filteredJobs =
+        statuses match {
+          case None => allJobs
+          case Some(allowed) => allJobs.filter(j => allowed.contains(j.state))
+        }
+      filteredJobs.sortBy(_.startTime.getMillis)(Ordering[Long].reverse)
+        .flatMap(j => refreshBinary(j))
+    }
   }
 
-  override def saveJobConfig(id: String, config: Config): Future[Boolean] = {
-    throw new NotImplementedError()
+  /*
+   * Helpers
+   */
+
+  private def readJobInfo(jobId: String): Option[JobInfo] = {
+    zookeeperUtils.read[JobInfo](s"$jobsDir/$jobId") match {
+      case None =>
+        logger.info(s"Didn't find any job information for the path: $jobsDir/$jobId")
+        None
+
+      case Some(j) => {
+        Some(j)
+      }
+    }
+
   }
 
-  override def getJobConfig(id: String): Future[Option[Config]] = {
-    throw new NotImplementedError()
+  /**
+   * Within JobInfo only an initial copy of the binary info is stored
+   * to identify the original BinaryInfo stored elsewhere
+   *
+   * The jobInfo shall only be returned if the BinaryInfo elsewhere still exists
+   */
+  private def refreshBinary(j: JobInfo): Option[JobInfo] = {
+    zookeeperUtils.read[Seq[BinaryInfo]](s"$binariesDir/" + j.binaryInfo.appName) match {
+      case Some(infoForBinary) =>
+        // identified by name AND uploadTime
+        infoForBinary.find(_.uploadTime.getMillis == j.binaryInfo.uploadTime.getMillis) match {
+          case Some(_) => Some(j)
+          case None =>
+            logger.info(s"Didn't find a binary for name and uploadtime: ("
+              + j.binaryInfo.appName + "," + j.binaryInfo.uploadTime + ")")
+            None
+        }
+      case None =>
+        logger.info(s"Didn't find a binary for name " + j.binaryInfo.appName)
+        None
+    }
   }
+
+  /*
+   * JobConfigs
+   */
+
+  override def saveJobConfig(jobId: String, config: Config): Future[Boolean] = {
+    Future {
+          val configRender = config.root().render(ConfigRenderOptions.concise())
+          logger.debug(s"Saving or updating job config under: $jobsDir/$jobId/config")
+          zookeeperUtils.write(configRender, s"$jobsDir/$jobId/config")
+    }
+  }
+
+  override def getJobConfig(jobId: String): Future[Option[Config]] = {
+    Future {
+      zookeeperUtils.read[String](s"$jobsDir/$jobId/config")
+        .flatMap(render => Some(ConfigFactory.parseString(render)))
+    }
+  }
+
+  /*
+   * Binaries
+   */
 
   override def getBinary(name: String): Future[Option[BinaryInfo]] = {
     Future {
@@ -103,7 +231,7 @@ class MetaDataZookeeperDAO(config: Config) extends MetaDataDAO {
 
   override def getBinariesByStorageId(storageId: String): Future[Seq[BinaryInfo]] = {
     Future {
-      zookeeperUtils.list(s"$binariesDir").flatMap(
+      zookeeperUtils.list(binariesDir).flatMap(
         name => zookeeperUtils.read[Seq[BinaryInfo]](s"$binariesDir/$name").getOrElse(Seq.empty[BinaryInfo])
       ).filter(_.binaryStorageId.getOrElse("") == storageId).
         groupBy(_.appName).map(_._2.head).toSeq
