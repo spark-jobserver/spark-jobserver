093ce25a0990470133dc3e6dbbcb02805c98f1fd
Behroz Sikander <behroz.sikander@sap.com>
2019-04-04 11:50:41 +0200

    fix(tests): Increase test timeouts
    Build servers are shared env with limited resources. Due to the load
    process can be slowed down. Due to this issue, multiple SJS test
    cases were giving timeout exceptions. Akka provides some settings
    that you can set to increase the timeouts of all of your testcaes.
    This change adds these configurations.

--- job-server/src/test/scala/spark/jobserver/JobSpecBase.scala
+++ job-server/src/test/scala/spark/jobserver/JobSpecBase.scala
@@ -38,6 +38,7 @@ trait JobSpecConfig {
       "spark.master" -> "local[*]",
       "context-factory" -> contextFactory,
       "spark.context-settings.test" -> "",
+      "akka.test.timefactor" -> 2,
       "spark.driver.allowMultipleContexts" -> true
     )
     ConfigFactory.parseMap(ConfigMap.asJava).withFallback(ConfigFactory.defaultOverrides())
