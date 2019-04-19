b361c079f7b3ab1fa3d2783ebbf987d0a385fab3
Sébastien Gautrin <sgautrin@users.noreply.github.com>
2019-04-19 14:26:35 +0200

    fix(jobserver): ensure job cleanup after fatal error - ref #1161
    `Future` only completes on success or when encountering a `NonFatal`
    error; when encountering a fatal error, the future does not complete at
    all and the error is passed to the execution context.
    
    This change wraps all `Throwable` so the `Future` can complete and the
    `Failure` handlers can cleanup the job and properly mark it as failed.

--- job-server/src/test/scala/spark/jobserver/JobManagerActorSpec.scala
+++ job-server/src/test/scala/spark/jobserver/JobManagerActorSpec.scala
@@ -225,6 +225,16 @@ class JobManagerActorSpec extends JobSpecBase(JobManagerActorSpec.getNewSystem) 
       errorMsg.err.getClass should equal (classOf[IllegalArgumentException])
     }
 
+    it("should return error if job throws a fatal error") {
+      manager ! JobManagerActor.Initialize(contextConfig, None, emptyActor)
+      expectMsgClass(initMsgWait, classOf[JobManagerActor.Initialized])
+
+      uploadTestJar()
+      manager ! JobManagerActor.StartJob("demo", classPrefix + "MyFatalErrorJob", emptyConfig, errorEvents)
+      val errorMsg = expectMsgClass(startJobWait, classOf[JobErroredOut])
+      errorMsg.err.getClass should equal (classOf[OutOfMemoryError])
+    }
+
     it("job should get jobConfig passed in to StartJob message") {
       val jobConfig = ConfigFactory.parseString("foo.bar.baz = 3")
       manager ! JobManagerActor.Initialize(contextConfig, None, emptyActor)
