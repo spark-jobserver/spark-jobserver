8aa62b350b1ec63147302298e7c6d9b5753b63c7
Valentina Glagoleva <valentina.glagoleva@sap.com>
2019-06-04 17:25:44 +0200

    feat(akka): Add split brain resolver
    Basic split brain resolver implementation specific for
    Jobserver.
    
    Downing provider analyzes by whom was it called (master/driver).
    For the case of drive and supervised cluster mode, it will
    make sys.exit(-1) on split brain resolution.
    For the rest of the cases shutdown of the actor system
    should be triggered.
    
    Down-if-alone is not configurable and is "on"
    by default. If only the oldest node becomes unreachable,
    it will be downed by other nodes.
    
    This split brain resolver uses "preferred roles" logic.
    It will try to find the oldest node with specified role, but
    if there are no nodes with this role left in the cluster,
    it will take just the oldest node.
    This allows contexts to run and maintain a healthy cluster
    even without jobserver masters.
    
    Change-Id: I8ee8cd8b9810d7a2b797b330a2dad4acf76e0a85

---
+++ job-server/src/main/scala/spark/jobserver/util/JobServerRoles.scala
@@ -0,0 +1,7 @@
+package spark.jobserver.util
+
+object JobServerRoles {
+  val propertyName = "jobserver.current-user"
+  val jobserverMaster = "jobserver-master"
+  val jobserverSlave = "jobserver-slave"
+}
