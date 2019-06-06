584415d5b8aec63ab8b6ee01fc500f7f1ec367f7
Behroz Sikander <behroz.sikander@sap.com>
2019-06-06 12:04:45 +0200

    fix(tests): Fix saveJobConfig tests (#1210)
    saveJobConfig function in JobDAO has Unit return
    value. This means that if we save a config
    and read it again in next line then it boils down
    to timing and the test becomes flaky.
    
    This change adds a retry while reading the config
    to fix the test.
    
    Further, SqlCommonSpec uses the saveJobConfig from
    SqlCommon which has a return value. So, in this
    test, we just add Await.
    
    Change-Id: Ia985d7b49652ce872094cde8f0144e2437eea403

--- job-server/src/main/scala/spark/jobserver/util/Utils.scala
+++ job-server/src/main/scala/spark/jobserver/util/Utils.scala
@@ -1,6 +1,7 @@
 package spark.jobserver.util
 
 import java.io.{Closeable, File}
+import scala.util.{Failure, Success, Try}
 
 object Utils {
   def usingResource[A <: Closeable, B](resource: A)(f: A => B): B = {
@@ -21,6 +22,16 @@ object Utils {
       if (!folder.mkdirs()) {
         throw new RuntimeException(s"Could not create directory $folder")
       }
+    }
+  }
+
+  def retry[T](n: Int, retryDelayInMs: Int = 500)(fn: => T): T = {
+    Try { fn } match {
+      case Success(x) => x
+      case _ if n > 1 =>
+        Thread.sleep(retryDelayInMs)
+        retry(n - 1)(fn)
+      case Failure(e) => throw e
     }
   }
 }
