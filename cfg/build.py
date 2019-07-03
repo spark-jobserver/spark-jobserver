025a2d4c37b27f4727338f8687e7bdd13a2044a8
Behroz Sikander <behroz.sikander@sap.com>
2019-07-03 15:02:07 +0200

    internal(xmake): Remove 0.13.12 folders
    Change-Id: Ib85afa280b8f09c26757e9df256e22517f75c358

--- cfg/build.py
+++ cfg/build.py
@@ -1,4 +1,4 @@
-#build scripts must define a class 'build'
+# build scripts must define a class 'build'
 import os
 from os.path import join
 from spi import BuildPlugin
@@ -23,6 +23,10 @@ class build(BuildPlugin):
     # Called when the actual build step is executed.
     def run(self):
         log.info("TRACE", "entering", "run")
+
+        log.info(">>>>>> Component dir is " + self.build_cfg.component_dir())
+        self.remove_old_sbt_artifacts(self.build_cfg.component_dir())
+
         self.importSbt()
         os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
         commandOutput = self.buildSJS()
@@ -48,6 +52,25 @@ class build(BuildPlugin):
         self._sbtexecutable = join(self._sbtbin,'sbt')
         log.info("TRACE", "exiting", "importSbt")
 
+        log.info(">>>> Listing org.scala-sbt folder", os.listdir("/data/xtr/MS/org.scala-sbt/"))
+        log.info(">>>> Listing sbt folder", os.listdir("/data/xtr/MS/org.scala-sbt/sbt/"))
+        log.info(">>>> Listing 1.2.8 folder", os.listdir("/data/xtr/MS/org.scala-sbt/sbt/1.2.8/"))
+        log.info(">>>> Listing sbt-1.2.8 folder", os.listdir("/data/xtr/MS/org.scala-sbt/sbt/1.2.8/sbt"))
+        log.info(">>>> import dir ", self.build_cfg.import_dir())
+        log.info(">>>> src dir ", self.build_cfg.src_dir())
+        log.info(">>>> runtime dir ", self.build_cfg.runtime())
+        log.info(">>>> component dir ", self.build_cfg.component_dir())
+        log.info(">>>> component dir ", os.listdir(join(self.build_cfg.component_dir(), "project")))
+
+        if os.path.isdir("~/.sbt/"):
+            log.info(">>>> sbt ", os.listdir("~/.sbt/"))
+
+        if os.path.isdir("~/.ivy2/"):
+            log.info(">>>> ivy2 ", os.listdir("~/.ivy2/"))
+
+        if os.path.isdir("~/.ivy/"):
+            log.info(">>>> ivy ", os.listdir("~/.ivy/"))
+
     def buildSJS(self):
         log.info("TRACE", "entering", "buildSJS")
         log.info("INFO: building SJS")
@@ -68,3 +91,22 @@ class build(BuildPlugin):
 
         log.info("TRACE", "exiting", "executeSbtCommand")
         return result
+
+    def remove_old_sbt_artifacts(self, project_dir):
+        self.remove_directory(join(project_dir, "project/target/"))
+        self.remove_directory(join(project_dir, "project/.sbtboot/"))
+        self.remove_directory(join(project_dir, "project/.boot/"))
+        self.remove_directory(join(project_dir, "project/.ivy/"))
+        self.remove_directory(join(project_dir, "project/.ivy2/"))
+        self.remove_directory(join(project_dir, "project/.sbt/boot/"))
+        self.remove_directory(join(project_dir, "project/project/"))
+        self.remove_directory('~/.sbt/')
+        self.remove_directory('~/.ivy2/')
+        self.remove_directory('~/.ivy/')
+
+
+    def remove_directory(self, path):
+        log.info(">>> Checking path for deleting " + path)
+        if os.path.isdir(path):
+            log.info(">>> DELETING directory " + path)
+            shutil.rmtree(path)
