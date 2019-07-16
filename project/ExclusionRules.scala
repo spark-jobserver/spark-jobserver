21c1283ab82bc1c02f7648e25a459d252875f531
Behroz Sikander <behroz.sikander@sap.com>
2019-07-16 11:55:07 +0200

    misc(all): Reenable sbt 1.1.6 and update sbt plugins
    Change-Id: I1dbb049fa97500ab168fbeab73093f80a04700b0

--- project/ExclusionRules.scala
+++ project/ExclusionRules.scala
@@ -5,6 +5,7 @@ object ExclusionRules {
   val excludeJackson = ExclusionRule(organization = "org.codehaus.jackson")
   val excludeScalaTest = ExclusionRule(organization = "org.scalatest")
   val excludeScala = ExclusionRule(organization = "org.scala-lang")
+  val excludeNettyIo = ExclusionRule(organization = "io.netty").withArtifact("netty-all")
   val excludeAsm = ExclusionRule(organization = "asm")
   val excludeQQ = ExclusionRule(organization = "org.scalamacros")
   val excludeJpountz = ExclusionRule(organization = "net.jpountz.lz4", name = "lz4")
