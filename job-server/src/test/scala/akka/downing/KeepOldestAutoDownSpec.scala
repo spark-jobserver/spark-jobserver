a5e90246537bbafa9f7b208c8f7bcb7994c28de0
Valentina Glagoleva <valentina.glagoleva@sap.com>
2019-08-08 08:28:43 +0200

    fix(sbr): Joining nodes should be considered healthy by default
    If the node has status Joining it should not be seen as unreachable
    without UnreachableMember event.
    
    Worst case scenario:
    Two nodes in the cluster, plus some responsive joining nodes.
    Second-oldest going down. Until now, the oldest node would consider
    itself as down & alone, shutting itself down and the joining nodes
    as well.
    With joining == healthy the oldest stays alive and the cluster
    intact with second-oldest removed out of the cluster and two joining
    nodes allowed to join after the removal.
    
    Change-Id: I2588cd42bf3f6072d9d4d9d0427452157e30215d

--- job-server/src/test/scala/akka/downing/KeepOldestAutoDownSpec.scala
+++ job-server/src/test/scala/akka/downing/KeepOldestAutoDownSpec.scala
@@ -314,11 +314,32 @@ class OldestAutoDowningSpec extends TestKit(ActorSystem("OldestAutoDowningSpec")
     it("should not down the node if not the oldest and oldest is alone unreachable") {
       val othersNodeDowningProvider = system.actorOf(Props(
         new OldestAutoDownTestActor(
-          initialMembersByAge.drop(3).head.address, 1.seconds, None, testActor
+          someMember.address, 1.seconds, None, testActor
         )))
       othersNodeDowningProvider ! CurrentClusterState(members = initialMembersByAge)
       othersNodeDowningProvider ! UnreachableMember(initialMembersByAge.head)
       expectNoMsg(3.seconds)
+    }
+
+    it("should not down the node if not the oldest and oldest is alone unreachable even though there are" +
+      " some nodes in Joining state") {
+      val othersNodeDowningProvider = system.actorOf(Props(
+        new OldestAutoDownTestActor(
+          someMember.address, 1.seconds, None, testActor
+        )))
+      val joiningMember = TestMember(Address("akka.tcp", "sys", "joining", 2552), Joining, Set("slave"))
+      othersNodeDowningProvider ! CurrentClusterState(members = initialMembersByAge)
+      othersNodeDowningProvider ! ReachableMember(joiningMember)
+      othersNodeDowningProvider ! UnreachableMember(initialMembersByAge.head)
+      expectNoMsg(3.seconds)
+    }
+
+    it("should down joining node if it becomes unreachable after a while") {
+      val joiningMember = TestMember(Address("akka.tcp", "sys", "joining", 2552), Joining, Set("slave"))
+      oldestDowningProvider ! CurrentClusterState(members = initialMembersByAge)
+      oldestDowningProvider ! ReachableMember(joiningMember)
+      oldestDowningProvider ! UnreachableMember(joiningMember)
+      expectMsg(DownCalled(joiningMember.address))
     }
   }
 
