1eeb8b3148c1a005766fe8d5aa18d44f9187d9ce
Robin Wolters <robin.wolters@sap.com>
2019-08-20 16:35:35 +0200

    feat(integration-tests): Test context synchronization in HA setup
    Test if creation and deletion of contexts are shared across two
    jobservers, i.e. if creation/deletion on one instance can be
    observed on the other one immediately
    
    Change-Id: I2bcf7e547d4784c2684fafcc0d92fb2da3c799a5

--- job-server-integration-tests/src/main/scala/spark/jobserver/integrationtests/IntegrationTests.scala
+++ job-server-integration-tests/src/main/scala/spark/jobserver/integrationtests/IntegrationTests.scala
@@ -5,19 +5,21 @@ import org.scalatest.ConfigMap
 object IntegrationTests extends App {
 
   // Parse args
-  if(args.length < 1){
+  if(args.length != 2){
     printUsage()
     sys.exit(-1)
   }
   val sjs1 = args(0)
+  val sjs2 = args(1)
 
   // Run all integration tests
   (new BasicApiTests).execute(configMap = ConfigMap(("address", sjs1)))
   (new CornerCasesTests).execute(configMap = ConfigMap(("address", sjs1)))
+  (new HATests).execute(configMap = ConfigMap(("sjs1", sjs1), ("sjs2", sjs2)))
 
   // Usage
   def printUsage(){
-    println("Usage: IntegrationTests <jobserver-ip>:<jobserver-port>")
+    println("Usage: IntegrationTests <jobserver1-ip>:<jobserver1-port> <jobserver2-ip>:<jobserver2-port>")
   }
 
 }
---
+++ job-server-integration-tests/src/test/scala/spark/jobserver/integrationtests/HATests.scala
@@ -0,0 +1,85 @@
+package spark.jobserver.integrationtests
+
+import org.scalatest.BeforeAndAfterAllConfigMap
+import org.scalatest.ConfigMap
+import org.scalatest.FreeSpec
+import org.scalatest.Matchers
+
+import com.softwaremill.sttp._
+import play.api.libs.json.Json
+
+class HATests extends FreeSpec with Matchers with BeforeAndAfterAllConfigMap{
+
+  // Configuration
+  var SJS1 = ""
+  var SJS2 = ""
+  implicit val backend = HttpURLConnectionBackend()
+  override def beforeAll(configMap: ConfigMap) = {
+    SJS1 = configMap.getWithDefault("sjs1", "localhost:8090")
+    SJS2 = configMap.getWithDefault("sjs2", "localhost:8091")
+  }
+
+  "Synchronize contexts across jobservers" - {
+
+    val context1 = "HAIntegrationTestContext1"
+    val context2 = "HAIntegrationTestContext2"
+
+    "POST /context on sjs1 should be visible on sjs2" in {
+      // Create on SJS1
+      val response1 = sttp.post(uri"$SJS1/contexts/$context1").send()
+      response1.code should equal(200)
+      val json1 = Json.parse(response1.body.merge)
+      (json1 \ "status").as[String] should equal("SUCCESS")
+      // Read from SJS2
+      val response2 = sttp.get(uri"$SJS2/contexts/$context1").send()
+      response2.code should equal(200)
+      val json2 = Json.parse(response2.body.merge)
+      (json2 \ "name").as[String] should equal(context1)
+      (json2 \ "state").as[String] should equal("RUNNING")
+    }
+
+    "POST /context on sjs2 should be visible on sjs1" in {
+      // Create on SJS2
+      val response1 = sttp.post(uri"$SJS2/contexts/$context2").send()
+      response1.code should equal(200)
+      val json1 = Json.parse(response1.body.merge)
+      (json1 \ "status").as[String] should equal("SUCCESS")
+      // Read from SJS1
+      val response2 = sttp.get(uri"$SJS1/contexts/$context2").send()
+      response2.code should equal(200)
+      val json2 = Json.parse(response2.body.merge)
+      (json2 \ "name").as[String] should equal(context2)
+      (json2 \ "state").as[String] should equal("RUNNING")
+    }
+
+    "DELETE /context on sjs1 should be visible on sjs2" in {
+      // DELETE on SJS1
+      val response1 = sttp.delete(uri"$SJS1/contexts/$context2").send()
+      response1.code should equal(200)
+      val json1 = Json.parse(response1.body.merge)
+      (json1 \ "status").as[String] should equal("SUCCESS")
+      // Read from SJS2
+      val response2 = sttp.get(uri"$SJS2/contexts/$context2").send()
+      response2.code should equal(200)
+      val json2 = Json.parse(response2.body.merge)
+      (json2 \ "name").as[String] should equal(context2)
+      (json2 \ "state").as[String] should equal("FINISHED")
+    }
+
+    "DELETE /context on sjs2 should be visible on sjs1" in {
+      // DELETE on SJS2
+      val response1 = sttp.delete(uri"$SJS2/contexts/$context1").send()
+      response1.code should equal(200)
+      val json1 = Json.parse(response1.body.merge)
+      (json1 \ "status").as[String] should equal("SUCCESS")
+      // Read from SJS1
+      val response2 = sttp.get(uri"$SJS1/contexts/$context1").send()
+      response2.code should equal(200)
+      val json2 = Json.parse(response2.body.merge)
+      (json2 \ "name").as[String] should equal(context1)
+      (json2 \ "state").as[String] should equal("FINISHED")
+    }
+
+  }
+
+}
