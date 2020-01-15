32afec9ca6c749c0d81c5e6fa4a4260883c9c80a
Behroz Sikander <behroz.sikander@sap.com>
2020-01-15 10:52:07 +0100

    fix(ci): Update CI scripts to 2.4.4
    Change-Id: Ic5128c5f306250c289f8c59d22f53d31bf87674e

--- ci/install-spark.sh
+++ ci/install-spark.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-curl -L -o /tmp/spark.tgz http://archive.apache.org/dist/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz
+curl -L -o /tmp/spark.tgz http://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
 tar -xvzf /tmp/spark.tgz -C /tmp
