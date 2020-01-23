40b197473edbc10c73b5e0a6f21aa501872e47eb
Valentina Glagoleva <valentina.glagoleva@sap.com>
2020-01-23 15:04:39 +0100

    refactor(dao): Remove SqlCommon from MetaDataSqlDAO
    This commit is first step of the removal of JobSqlDAO and SqlCommon
    classes.
    
    As JobSqlDAO is planned to be deprecated, there is no need anymore
    for SqlCommon class and sharing of the code between MetaDataSqlDAO
    and JobSqlDAO. Duplicating first the code back to MetaDataSqlDAO
    will make future deletion commits easier and cleaner.
    
    Change-Id: I69f61a6689aceaee57cec9028d40e9ddf30b981d

--- job-server/src/main/scala/spark/jobserver/io/MetaDataSqlDAO.scala
+++ job-server/src/main/scala/spark/jobserver/io/MetaDataSqlDAO.scala
@@ -6,28 +6,160 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
 import scala.reflect.runtime.universe
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigFactory, ConfigRenderOptions}
 import org.joda.time.DateTime
 import org.slf4j.LoggerFactory
 import slick.driver.JdbcProfile
+import spark.jobserver.util.SqlDBUtils
 
 class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
   private val logger = LoggerFactory.getLogger(getClass)
-  val sqlCommon: SqlCommon = new SqlCommon(config)
 
+  val dbUtils = new SqlDBUtils(config)
   val runtimeMirror = universe.runtimeMirror(getClass.getClassLoader)
-  val profile = runtimeMirror.reflectModule(sqlCommon.profileModule).instance.asInstanceOf[JdbcProfile]
+  val profile = runtimeMirror.reflectModule(dbUtils.profileModule).instance.asInstanceOf[JdbcProfile]
 
+  private val timeout = 10.seconds
   import profile.api._
+  // Definition of the tables
+  //scalastyle:off
+  // Explicitly avoiding to label 'jarId' as a foreign key to avoid dealing with
+  // referential integrity constraint violations.
+  class Jobs(tag: Tag) extends Table[(String, String, String, String, String, String, String, Timestamp,
+    Option[Timestamp], Option[String], Option[String], Option[String])](tag, "JOBS") {
+    def jobId = column[String]("JOB_ID", O.PrimaryKey)
+    def contextId = column[String]("CONTEXT_ID")
+    def contextName = column[String]("CONTEXT_NAME")
+    def binIds = column[String]("BIN_IDS")
+    def URIs = column[String]("URIS")
+    def classPath = column[String]("CLASSPATH")
+    def state = column[String]("STATE")
+    def startTime = column[Timestamp]("START_TIME")
+    def endTime = column[Option[Timestamp]]("END_TIME")
+    def error = column[Option[String]]("ERROR")
+    def errorClass = column[Option[String]]("ERROR_CLASS")
+    def errorStackTrace = column[Option[String]]("ERROR_STACK_TRACE")
+    def * = (jobId, contextId, contextName, binIds, URIs, classPath, state, startTime, endTime,
+      error, errorClass, errorStackTrace)
+  }
 
-  sqlCommon.initFlyway()
+  val jobs = TableQuery[Jobs]
+
+  class Contexts(tag: Tag) extends Table[(String, String, String, Option[String], Timestamp,
+    Option[Timestamp], String, Option[String])](tag, "CONTEXTS") {
+    def id = column[String]("ID", O.PrimaryKey)
+    def name = column[String]("NAME")
+    def config = column[String]("CONFIG")
+    def actorAddress = column[Option[String]]("ACTOR_ADDRESS")
+    def startTime = column[Timestamp]("START_TIME")
+    def endTime = column[Option[Timestamp]]("END_TIME")
+    def state = column[String]("STATE")
+    def error = column[Option[String]]("ERROR")
+    def * = (id, name, config, actorAddress, startTime, endTime, state, error)
+  }
+
+  val contexts = TableQuery[Contexts]
+
+  class Configs(tag: Tag) extends Table[(String, String)](tag, "CONFIGS") {
+    def jobId = column[String]("JOB_ID", O.PrimaryKey)
+    def jobConfig = column[String]("JOB_CONFIG")
+    def * = (jobId, jobConfig)
+  }
+
+  val configs = TableQuery[Configs]
+
+  class Binaries(tag: Tag) extends Table[(Int, String, String, Timestamp, Array[Byte])](tag, "BINARIES") {
+    def binId = column[Int]("BIN_ID", O.PrimaryKey, O.AutoInc)
+    def appName = column[String]("APP_NAME")
+    def binaryType = column[String]("BINARY_TYPE")
+    def uploadTime = column[Timestamp]("UPLOAD_TIME")
+    def binHash = column[Array[Byte]]("BIN_HASH")
+    def * = (binId, appName, binaryType, uploadTime, binHash)
+  }
+
+  val binaries = TableQuery[Binaries]
+
+  // Convert from joda DateTime to java.sql.Timestamp
+  def convertDateJodaToSql(dateTime: DateTime): Timestamp = new Timestamp(dateTime.getMillis)
+
+  // Convert from java.sql.Timestamp to joda DateTime
+  def convertDateSqlToJoda(timestamp: Timestamp): DateTime = new DateTime(timestamp.getTime)
+
+  private def contextInfoFromRow(row: (String, String, String, Option[String],
+    Timestamp, Option[Timestamp], String, Option[String])): ContextInfo = row match {
+    case (id, name, config, actorAddress, start, end, state, error) =>
+      ContextInfo(
+        id,
+        name,
+        config,
+        actorAddress,
+        convertDateSqlToJoda(start),
+        end.map(convertDateSqlToJoda),
+        state,
+        error.map(new Throwable(_))
+      )
+  }
+
+  def queryBinaryId(name: String, binaryType: BinaryType, uploadTime: DateTime): Future[Int] = {
+    val dateTime = convertDateJodaToSql(uploadTime)
+    val query = binaries.filter { bin =>
+      bin.appName === name && bin.uploadTime === dateTime && bin.binaryType === binaryType.name
+    }.map(_.binId).result
+    dbUtils.db.run(query.head)
+  }
+
+  def jobInfoFromRow(row: (String, String, String, String, String, String, String,
+    Timestamp, Option[Timestamp], Option[String], Option[String], Option[String])): JobInfo = row match {
+    case (id, contextId, contextName, bins, uris, classpath,
+    state, start, end, err, errCls, errStTr) =>
+      val errorInfo = err.map(ErrorData(_
+        , errCls.getOrElse(""), errStTr.getOrElse("")))
+
+      val binIds = bins.split(",").toSeq.map(_.toInt)
+      val query = binaries.filter(_.binId inSet binIds).result
+      val binInfos = Await.result(
+        dbUtils.db.run(query).map(r => r.map(binaryInfoFromRow(_))), timeout)
+      val uriInfos = if (uris.nonEmpty) {
+        uris.split(",").toSeq.map(u => BinaryInfo(u, BinaryType.URI, DateTime.now()))
+      } else {
+        Seq.empty
+      }
+      JobInfo(
+        id,
+        contextId,
+        contextName,
+        classpath,
+        state,
+        convertDateSqlToJoda(start),
+        end.map(convertDateSqlToJoda),
+        errorInfo,
+        binInfos ++ uriInfos
+      )
+  }
+
+  dbUtils.initFlyway()
 
   /** Persist a context info.
     *
     * @param contextInfo
     */
   def saveContext(contextInfo: ContextInfo): Future[Boolean] = {
-    sqlCommon.saveContext(contextInfo)
+    val startTime = convertDateJodaToSql(contextInfo.startTime)
+    val endTime = contextInfo.endTime.map(t => convertDateJodaToSql(t))
+    val errors = contextInfo.error.map(e => e.getMessage)
+    val row = (contextInfo.id, contextInfo.name, contextInfo.config,
+      contextInfo.actorAddress, startTime, endTime, contextInfo.state, errors)
+    for {
+      result <- dbUtils.db.run(contexts.insertOrUpdate(row))
+    } yield {
+      if (result == 0) {
+        val e = new SlickException(s"Could not update ${contextInfo.id} in the database")
+        logger.error(e.getMessage, e)
+        false
+      } else {
+        true
+      }
+    }
   }
 
   /**
@@ -36,7 +168,8 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @return
     */
   def getContext(id: String): Future[Option[ContextInfo]] = {
-    sqlCommon.getContext(id)
+    val query = contexts.filter(_.id === id).result
+    dbUtils.db.run(query.headOption).map(r => r.map(contextInfoFromRow(_)))
   }
 
   /**
@@ -45,7 +178,8 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @return
     */
   def getContextByName(name: String): Future[Option[ContextInfo]] = {
-    sqlCommon.getContextByName(name)
+    val query = contexts.filter(_.name === name).sortBy(_.startTime.desc).result
+    dbUtils.db.run(query.headOption).map(r => r.map(contextInfoFromRow(_)))
   }
 
   /**
@@ -56,7 +190,18 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     */
   def getContexts(limit: Option[Int] = None, statuses: Option[Seq[String]] = None):
   Future[Seq[ContextInfo]] = {
-    sqlCommon.getContexts(limit, statuses)
+    val query = statuses match {
+      case Some(statuses) => contexts.filter(_.state.inSet(statuses))
+      case None => contexts
+    }
+    val sortQuery = query.sortBy(_.startTime.desc)
+    val limitQuery = limit match {
+      case Some(i) => sortQuery.take(i)
+      case None => sortQuery
+    }
+    for (r <- dbUtils.db.run(limitQuery.result)) yield {
+      r.map(contextInfoFromRow)
+    }
   }
 
   /**
@@ -67,22 +212,22 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
   def saveJob(jobInfo: JobInfo): Future[Boolean] = {
     val jarIds = jobInfo.cp.filter(_.binaryType != BinaryType.URI).map(b => {
       Await.result(
-        sqlCommon.queryBinaryId(
+        queryBinaryId(
           b.appName,
           b.binaryType,
           b.uploadTime),
         60.seconds)
     }).mkString(",")
     val uris = jobInfo.cp.filter(_.binaryType == BinaryType.URI).map(_.appName).mkString(",")
-    val startTime = sqlCommon.convertDateJodaToSql(jobInfo.startTime)
-    val endTime = jobInfo.endTime.map(t => sqlCommon.convertDateJodaToSql(t))
+    val startTime = convertDateJodaToSql(jobInfo.startTime)
+    val endTime = jobInfo.endTime.map(t => convertDateJodaToSql(t))
     val error = jobInfo.error.map(e => e.message)
     val errorClass = jobInfo.error.map(e => e.errorClass)
     val errorStackTrace = jobInfo.error.map(e => e.stackTrace)
     val row = (jobInfo.jobId, jobInfo.contextId, jobInfo.contextName, jarIds, uris, jobInfo.mainClass,
       jobInfo.state, startTime, endTime, error, errorClass, errorStackTrace)
     val result = for {
-      result <- sqlCommon.db.run(sqlCommon.jobs.insertOrUpdate(row))
+      result <- dbUtils.db.run(jobs.insertOrUpdate(row))
     } yield {
       if (result == 0) {
         val e = new SlickException(s"Could not update ${jobInfo.jobId} in the database")
@@ -101,7 +246,9 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @return
     */
   def getJob(id: String): Future[Option[JobInfo]] = {
-    sqlCommon.getJob(id)
+    for (r <- dbUtils.db.run(jobs.filter(j => j.jobId === id).result)) yield {
+      r.map(jobInfoFromRow).headOption
+    }
   }
 
   /**
@@ -110,14 +257,32 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @return
     */
   def getJobs(limit: Int, status: Option[String] = None): Future[Seq[JobInfo]] = {
-    sqlCommon.getJobs(limit, status)
+    val baseQuery = if (status.isDefined) {
+      jobs.filter(_.state === status)
+    } else {
+      jobs
+    }
+    val limitQuery = baseQuery.sortBy(_.startTime).take(limit)
+    // Transform the each row of the table into a map of JobInfo values
+    for (r <- dbUtils.db.run(limitQuery.result)) yield {
+      r.map(jobInfoFromRow)
+    }
   }
 
   /**
     * Return all job ids to their job info.
     */
   def getJobsByContextId(contextId: String, statuses: Option[Seq[String]] = None): Future[Seq[JobInfo]] = {
-    sqlCommon.getJobsByContextId(contextId, statuses)
+    val joinQuery = for {
+      j <- jobs if ((contextId, statuses) match {
+        case (contextId, Some(s)) =>
+          j.contextId === contextId && j.state.inSet(s)
+        case _ => j.contextId === contextId
+      })
+    } yield {
+      j
+    }
+    dbUtils.db.run(joinQuery.result).map(_.map(jobInfoFromRow))
   }
 
   /**
@@ -127,7 +292,18 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @param config
     */
   def saveJobConfig(id: String, config: Config): Future[Boolean] = {
-    sqlCommon.saveJobConfig(id, config)
+    val configRender = config.root().render(ConfigRenderOptions.concise())
+    for {
+      result <- (dbUtils.db.run(configs.map(c => c.*) += (id, configRender)))
+    } yield {
+      if (result == 0) {
+        val e = new SlickException(s"Could not insert $id into database")
+        logger.error(e.getMessage, e)
+        false
+      } else {
+        true
+      }
+    }
   }
 
   /**
@@ -135,7 +311,10 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @return
     */
   def getJobConfig(id: String): Future[Option[Config]] = {
-    sqlCommon.getJobConfig(id)
+    val query = configs
+      .filter(_.jobId === id).map(_.jobConfig).result
+
+    dbUtils.db.run(query.headOption).map(c => c.map(ConfigFactory.parseString(_)))
   }
 
   /**
@@ -144,8 +323,8 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @param name binary name
     */
   def getBinary(name: String): Future[Option[BinaryInfo]] = {
-    val query = sqlCommon.binaries.filter(_.appName === name).sortBy(_.uploadTime.desc).result
-    sqlCommon.db.run(query.headOption).map(r => r.map(binaryInfoFromRow(_)))
+    val query = binaries.filter(_.appName === name).sortBy(_.uploadTime.desc).result
+    dbUtils.db.run(query.headOption).map(r => r.map(binaryInfoFromRow(_)))
   }
 
   /**
@@ -154,19 +333,19 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @return
     */
   def getBinaries: Future[Seq[BinaryInfo]] = {
-    val query = sqlCommon.binaries.groupBy { r =>
+    val query = binaries.groupBy { r =>
       (r.appName, r.binaryType)
     }.map {
       case ((name, binaryType), bin) =>
         (name, binaryType, bin.map(_.uploadTime).max.get)
     }.result
-    for (m <- sqlCommon.db.run(query)) yield {
+    for (m <- dbUtils.db.run(query)) yield {
       m.map {
         case (appName, binaryType, uploadTime) =>
           BinaryInfo(
             appName,
             BinaryType.fromString(binaryType),
-            sqlCommon.convertDateSqlToJoda(uploadTime)
+            convertDateSqlToJoda(uploadTime)
           )
       }
     }
@@ -178,10 +357,10 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @return
     */
   def getBinariesByStorageId(storageId: String): Future[Seq[BinaryInfo]] = {
-    val query = sqlCommon.binaries.filter(
+    val query = binaries.filter(
           _.binHash === BinaryDAO.hashStringToBytes(storageId)
         ).result
-    for (m <- sqlCommon.db.run(query)) yield {
+    for (m <- dbUtils.db.run(query)) yield {
       m.map(binaryInfoFromRow)
     }
   }
@@ -198,9 +377,9 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
                  uploadTime: DateTime,
                  binaryStorageId: String): Future[Boolean] = {
     val hash = BinaryDAO.hashStringToBytes(binaryStorageId)
-    val dbAction = (sqlCommon.binaries +=
-      (-1, name, binaryType.name, sqlCommon.convertDateJodaToSql(uploadTime), hash))
-    sqlCommon.db.run(dbAction).map(_ == 1).recover(logDeleteErrors)
+    val dbAction = (binaries +=
+      (-1, name, binaryType.name, convertDateJodaToSql(uploadTime), hash))
+    dbUtils.db.run(dbAction).map(_ == 1).recover(logDeleteErrors)
   }
 
   /**
@@ -208,12 +387,23 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
     * @param name
     */
   def deleteBinary(name: String): Future[Boolean] = {
-    val deleteBinary = sqlCommon.binaries.filter(_.appName === name).delete
-    sqlCommon.db.run(deleteBinary).map(_ > 0).recover(logDeleteErrors)
+    val deleteBinary = binaries.filter(_.appName === name).delete
+    dbUtils.db.run(deleteBinary).map(_ > 0).recover(logDeleteErrors)
   }
 
   def getJobsByBinaryName(binName: String, statuses: Option[Seq[String]] = None): Future[Seq[JobInfo]] = {
-    sqlCommon.getJobsByBinaryName(binName, statuses)
+    val binQuery = binaries.filter(_.appName === binName).map(_.binId).result.headOption
+    val binId = Await.result(dbUtils.db.run(binQuery), 10.seconds).getOrElse(return Future.successful(Seq.empty))
+
+    // .result should be called on jobs before uris/binaries string can be parsed
+    // before it's only Rep[String], not real String
+    val jobsQuery = jobs.result.map(_.filter {
+      case (_, _, _, cp, _, _, status, _, _, _, _, _) =>
+        cp.split(",").map(_.toInt).contains(binId) &&
+          statuses.getOrElse(Seq(status)).contains(status)
+    })
+
+    dbUtils.db.run(jobsQuery).map(_.map(jobInfoFromRow))
   }
 
   private def binaryInfoFromRow(row: (Int, String, String, Timestamp, Array[Byte])): BinaryInfo = row match {
@@ -221,7 +411,7 @@ class MetaDataSqlDAO(config: Config) extends MetaDataDAO {
       BinaryInfo(
         appName,
         BinaryType.fromString(binaryType),
-        sqlCommon.convertDateSqlToJoda(uploadTime),
+        convertDateSqlToJoda(uploadTime),
         Some(BinaryDAO.hashBytesToString(hash))
       )
   }
