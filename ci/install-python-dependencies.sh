bac735570ad97517b9c54b443022f30121869944
Behroz Sikander <behroz.sikander@sap.com>
2020-02-17 10:44:12 +0100

    Test

--- ci/install-python-dependencies.sh
+++ ci/install-python-dependencies.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
-pip install --upgrade pip==9.0.1
+#python -m pip install --user --upgrade pip
+#pip install --upgrade pip==9.0.1
+#python -m pip install -U pip==9.0.1
+#pip install -U pip
+#pip install --upgrade pip==9.0.1
+pip install --upgrade pip
 pip install --user pyhocon
 pip3 install --user pyhocon
 pip install --user pep8
