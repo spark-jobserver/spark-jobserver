7a3f10dece227ebbefe1bf586d1eec7ee873827f
Marc Zoeller <m.zoeller@usu.de>
2021-03-15 18:16:55 +0100

    misc: Update libraries without breaking changes

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,38 +1,33 @@
-import scala.util.Properties.isJavaAtLeast
-
 object Versions {
-  lazy val spark = sys.env.getOrElse("SPARK_VERSION", "2.4.4")
+  lazy val spark = sys.env.getOrElse("SPARK_VERSION", "2.4.7")
 
   lazy val akka = "2.5.32"
-  lazy val akkaHttp = "10.1.13"
+  lazy val akkaHttp = "10.1.14"
   lazy val akkaHttpCors = "0.4.2"
-  lazy val cassandra = "3.3.0"
   lazy val commons = "1.4"
   lazy val derby = "10.12.1.1"
-  lazy val flyway = "3.2.1"
-  lazy val hadoop = "2.7.3"
+  lazy val flyway = "4.2.0"
+  lazy val hadoop = "2.7.7"
   lazy val h2 = "1.3.176"
   lazy val java = sys.env.getOrElse("JAVA_VERSION", "8-jdk")
   lazy val jjwt = "0.9.1"
   lazy val jwksRsa = "0.15.0"
   lazy val jodaConvert = "1.8.1"
   lazy val jodaTime = "2.9.3"
-  lazy val logback = "1.0.7"
-  lazy val mesos = sys.env.getOrElse("MESOS_VERSION", "1.0.0-2.0.89.ubuntu1404")
+  lazy val logback = "1.2.3"
   lazy val metrics = "2.2.0"
-  lazy val postgres = "9.4.1209"
-  lazy val mysql = "5.1.42"
-  lazy val py4j = "0.10.7"
-  lazy val scalaTest = "3.0.1"
-  lazy val scalatic = "3.0.1"
-  lazy val slf4j = "1.7.25"
-  lazy val mockito = "2.21.0"
-  lazy val shiro = "1.2.4"
+  lazy val postgres = "42.2.19"
+  lazy val mysql = "8.0.23"
+  lazy val py4j = "0.10.9.2"
+  lazy val scalaTest = "3.1.4"
+  lazy val scalactic = "3.1.4"
+  lazy val slf4j = "1.7.30"
+  lazy val mockito = "2.28.2"
+  lazy val shiro = "1.7.1"
   lazy val slick = "3.3.3"
-  lazy val typeSafeConfig = if (isJavaAtLeast("1.8")) "1.3.0" else "1.2.1"
-  lazy val cassandraConnector = "2.4.3"
+  lazy val typeSafeConfig = "1.4.1"
+  lazy val cassandraConnector = "2.5.1"
   lazy val curator = "4.2.0"
   lazy val curatorTest = "4.2.0"
-  lazy val xerces = "2.9.1"
   lazy val commonConfigurations = "1.10"
 }
