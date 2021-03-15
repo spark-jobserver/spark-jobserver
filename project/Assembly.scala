23cce1391d645a4bc3f44d8a2f1fec01624e0609
Marc Zoeller <m.zoeller@usu.de>
2021-03-15 09:25:57 +0100

    misc: Use latest sbt version

--- project/Assembly.scala
+++ project/Assembly.scala
@@ -4,17 +4,17 @@ import sbtassembly.AssemblyPlugin.autoImport._
 
 object Assembly {
   lazy val settings = Seq(
-    assemblyJarName in assembly := "spark-job-server.jar",
+    assembly / assemblyJarName := "spark-job-server.jar",
       // uncomment below to exclude tests
     // test in assembly := {},
-    assemblyExcludedJars in assembly := (fullClasspath in assembly).map(_.filter { cp =>
+    assembly / assemblyExcludedJars := (assembly / fullClasspath).map(_.filter { cp =>
       List("servlet-api", "guice-all", "junit", "uuid",
         "jetty", "jsp-api-2.0", "antlr", "avro", "slf4j-log4j", "log4j-1.2",
         "scala-actors", "commons-cli", "stax-api", "mockito").exists(cp.data.getName.startsWith(_))
     }).value,
     // We don't need the Scala library, Spark already includes it
-    assembleArtifact in assemblyPackageScala := false,
-    assemblyMergeStrategy in assembly := {
+    assemblyPackageScala / assembleArtifact := false,
+    assembly / assemblyMergeStrategy := {
       case m if m.toLowerCase.endsWith("manifest.mf") => MergeStrategy.discard
       case m if m.toLowerCase.matches("meta-inf.*\\.sf$") => MergeStrategy.discard
       case "reference.conf" => MergeStrategy.concat
@@ -23,7 +23,7 @@ object Assembly {
     // NOTE(velvia): Some users have reported NoClassDefFound errors due to this shading.
     // java.lang.NoClassDefFoundError: sjs/shapeless/Poly2$Case2Builder$$anon$3
     // This is disabled for now, if you really need it, re-enable it and good luck!
-    // assemblyShadeRules in assembly := Seq(
+    // assembly / assemblyShadeRules := Seq(
     //   ShadeRule.rename("shapeless.**" -> "sjs.shapeless.@1").inAll
     // )
   )
