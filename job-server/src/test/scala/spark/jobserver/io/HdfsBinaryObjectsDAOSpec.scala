935a24d3e16d7a5856f012b7ba9e7be06c563a50
Robin Wolters <robin.wolters@sap.com>
2021-05-28 08:58:45 +0200

    feat(dao): Implement deleteJobResults for HdfsBinaryObjectsDAO
    Implement and test deleteJobResults according to the
    BinaryObjectsDAO interface.
    
    Change-Id: Ie82b39393d4ab43e222e4309fbb15c8490542698

--- job-server/src/test/scala/spark/jobserver/io/HdfsBinaryObjectsDAOSpec.scala
+++ job-server/src/test/scala/spark/jobserver/io/HdfsBinaryObjectsDAOSpec.scala
@@ -71,26 +71,40 @@ class HdfsBinaryObjectsDAOSpec extends AnyFunSpec with Matchers with BeforeAndAf
     it("should write, get, delete the file from hdfs") {
       val validInvalidInput = "Ḽơᶉëᶆ ȋṕšᶙṁ � � � � test 1 4 4 1".toCharArray.map(_.toByte)
       Await.result(
-        hdfsDAO.getJobResult(testFileName), timeout) should equal (None) // ensure there is no file before
-      Await.result(hdfsDAO.saveJobResult(testFileName, validInvalidInput), timeout) should equal (true)
-      Await.result(hdfsDAO.getJobResult(testFileName), timeout).get should equal (validInvalidInput)
-      Await.result(hdfsDAO.deleteJobResult(testFileName), timeout) should equal (true)
+        hdfsDAO.getJobResult(testFileName), timeout) should equal(None) // ensure there is no file before
+      Await.result(hdfsDAO.saveJobResult(testFileName, validInvalidInput), timeout) should equal(true)
+      Await.result(hdfsDAO.getJobResult(testFileName), timeout).get should equal(validInvalidInput)
+      Await.result(hdfsDAO.deleteJobResults(Seq(testFileName)), timeout) should equal(true)
       Await.result(
-        hdfsDAO.getJobResult(testFileName), timeout) should equal (None) // ensure there is no file after
+        hdfsDAO.getJobResult(testFileName), timeout) should equal(None) // ensure there is no file after
     }
 
     it("should return nothing if file doesn't exist") {
-      Await.result(hdfsDAO.getJobResult(testFileName), timeout) should equal (None)
+      Await.result(hdfsDAO.getJobResult(testFileName), timeout) should equal(None)
     }
 
-    it("should return false if delete is unsuccessful") {
-      Await.result(hdfsDAO.deleteJobResult(testFileName), timeout) should equal (false)
+    it("should delete multiple job results") {
+      val result = "Result".toCharArray.map(_.toByte)
+      Await.result(hdfsDAO.saveJobResult("1", result), timeout) should equal(true)
+      Await.result(hdfsDAO.saveJobResult("2", result), timeout) should equal(true)
+      Await.result(hdfsDAO.saveJobResult("3", result), timeout) should equal(true)
+      Await.result(hdfsDAO.deleteJobResults(Seq("1", "2")), timeout) should equal(true)
+      Await.result(hdfsDAO.getJobResult("1"), timeout) should equal(None)
+      Await.result(hdfsDAO.getJobResult("2"), timeout) should equal(None)
+      Await.result(hdfsDAO.getJobResult("3"), timeout).get should equal(result)
+    }
+
+    it("should return false if delete is (partly) unsuccessful") {
+      val result = "Result".toCharArray.map(_.toByte)
+      Await.result(hdfsDAO.saveJobResult(testFileName, result), timeout) should equal(true)
+      Await.result(hdfsDAO.deleteJobResults(Seq(testFileName, "nonexistent")), timeout) should equal(false)
+      Await.result(hdfsDAO.getJobResult(testFileName), timeout) should equal(None)
     }
 
     it("should return true writing a file which already exists") {
       val testArray: Array[Byte] = "Test file".toCharArray.map(_.toByte)
-      Await.result(hdfsDAO.saveJobResult(testFileName, testArray), timeout) should equal (true)
-      Await.result(hdfsDAO.saveJobResult(testFileName, testArray), timeout) should equal (true)
+      Await.result(hdfsDAO.saveJobResult(testFileName, testArray), timeout) should equal(true)
+      Await.result(hdfsDAO.saveJobResult(testFileName, testArray), timeout) should equal(true)
     }
   }
 
